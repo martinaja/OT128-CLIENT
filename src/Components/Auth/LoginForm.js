8afa8a59a7831e26e474e0f107826eddc64df31b
Martin Aja <martinjoseaja@gmail.com>
2022-01-27 18:57:55 -0300

    Add Login form and routing

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,142 +1,56 @@
+
 import React from 'react';
+import '../FormStyles.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Button } from '@mui/material';
 
-import {
-Button,
-CssBaseline,
-TextField,
-FormControlLabel,
-Checkbox,
-
-Grid,
-Box, Typography, Container  } from '@material-ui/core';
-
-import {  Link } from "react-router-dom";
-
-import { makeStyles } from '@material-ui/core/styles';
-
-import * as yup from 'yup';
-
-import { useFormik } from 'formik';
-
-
-const useStyles = makeStyles((theme) => ({
-paper: {
-marginTop: theme.spacing(8),
-display: 'flex',
-flexDirection: 'column',
-alignItems: 'center',
-},
-avatar: {
-margin: theme.spacing(1),
-backgroundColor: theme.palette.secondary.main,
-},
-form: {
-width: '100%', // Fix IE 11 issue.
-marginTop: theme.spacing(1),
-},
-submit: {
-margin: theme.spacing(3, 0, 2),
-},
-}));
-
-const loginShema = yup.object({
-            email: yup.string()
-              .required('Your email please.')
-              .email('Enter a valid email.'),
-          password: yup.string()
-                 .required('Your password please.')
-                  .min(8, 'Your password is at least 6 characters long.')
-
-})
-
-
-
-function LoginForm() {
-const classes = useStyles();
-const formik = useFormik({
-initialValues:{
-  email:'',
-  password:''
-},
-validationSchema: loginShema,
-onSubmit: (values) => {
- alert(JSON.stringify(values, null, 2))
- },
-
-})
+const LoginForm = () => {
+ 
+    const validateFields = (values) => {    
+ const errors = {}
+ if (!values.email) {
+            errors.email = '* campo obligatorio'
+        }
+        if (!values.password) {
+            errors.password = '* campo obligatorio'
+        }
+       if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = '* direccion de email incorrecta';
+        }
+       if (values.password.length < 6) {
+            errors.password = '* a longitud minima es 6'
+        }
+        if (!/^(?=.*\d)(?=.*[a-zA-Záéíóúüñ])(?=.*[$-/:-?{-~!"^_`[\]])/.test(values.password)) {
+            errors.password = '* contraseña incorrecta'
+        }
+ return errors
+    }
+const handleSubmit = (values) => {
+const userInfo = {
+            user: values.email,
+            password: values.password
+        }
+    }
 return (
- <body className="circuit-background">
-  <Container component="main" maxWidth="xs" >
-   <CssBaseline />
-   <div className={classes.paper}>
-
-   <Typography component="h1" variant="h5">
-    Sign in
-   </Typography>
-  <form className={classes.form} noValidate>
-    <TextField
-      variant="filled"
-      margin="normal"
-      fullWidth
-      id="email"
-      label="Email Address"
-      name="email"
-      autoComplete="email"
-      autoFocus
-      onChange={formik.handleChange}
-      value={formik.values.email}
-      error={formik.touched.email && Boolean(formik.errors.email)}
-      helperText={formik.touched.email && formik.errors.email}
-
-    />
-    <TextField
-      variant="filled"
-      margin="normal"
-      fullWidth
-      name="password"
-      label="Password"
-      type="password"
-      id="password"
-      autoComplete="current-password"
-      onChange={formik.handleChange}
-      value={formik.values.password}
-      error={formik.touched.password && Boolean(formik.errors.password)}
-      helperText={formik.touched.password && formik.errors.password}
-
-
-    />
-    <FormControlLabel
-      control={<Checkbox value="remember" required color="primary" />}
-      label="Remember me"
-    />
-    <Button
-      type="submit"
-      fullWidth
-      variant="outlined"
-      color="primary"
-      className={classes.submit}
-    >
-      Sign In
-    </Button>
-    <Grid container>
-      <Grid item xs>
-        <Link >
-          Forgot password?
-        </Link>
-       </Grid>
-       <Grid item>
-         <Link to="/SignUp">
-           Don't have an account? Sign Up
-         </Link>
-       </Grid>
-     </Grid>
-   </form>
- </div>
- <Box mt={8}>
-</Box>
- </Container>
-</body>
-)
+       
+        <Formik
+            initialValues={{ email: '', password: '', confirmPassword: '' }}
+            validate={validateFields}
+            onSubmit={(values) => handleSubmit(values)}
+        >
+            {({ isSubmitting }) => (
+ <Form className='form-container'>
+                    <Field type="email" name="email" className='input-field' placeholder="Ingrese email" />
+                    <ErrorMessage name="email" component="div" />
+                    <Field type="password" name="password" className='input-field' placeholder="Ingrese contraseña" />
+                    <ErrorMessage name="password" component="div" />
+                    <Button color="inherit" type="submit-btn" disabled={isSubmitting}>
+                        Registrarse
+                    </Button>
+                </Form>
+            )}
+        </Formik>
+    );
 }
 
-export default LoginForm;
+export default LoginForm; 
