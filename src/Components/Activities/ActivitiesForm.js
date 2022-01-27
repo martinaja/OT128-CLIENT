957eb31507d8210dfeb15618b80e677f7f2e2fac
Frank076 <franco.776@hotmail.com>
2022-01-27 19:33:40 -0300

    added formik, yup and logic to handle errors

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,50 +1,67 @@
-import React, { createRef, useState } from 'react';
 import '../FormStyles.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const ActivitiesForm = () => {
-	const [initialValues, setInitialValues] = useState({
-		name: '',
-		description: ''
+	const ALLOWED_FORMATS = ['image/jpeg', 'image/png', 'image/jpg'];
+
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			description: '',
+			image: undefined
+		},
+		validationSchema: Yup.object({
+			name: Yup.string().required('Required'),
+			description: Yup.string().required('Required'),
+			image: Yup.mixed()
+				.test(
+					'fileType',
+					'File must be an image jpg or png',
+					value => {
+						if (value)
+							return ALLOWED_FORMATS.includes(value && value.type);
+					}
+				)
+				.required('Required')
+		}),
+		onSubmit: formData => {
+			console.log(formData);
+		}
 	});
-	const imageInput = createRef();
-
-	const handleChange = e => {
-		if (e.target.name === 'name') {
-			setInitialValues({ ...initialValues, name: e.target.value });
-		}
-		if (e.target.name === 'description') {
-			setInitialValues({
-				...initialValues,
-				description: e.target.value
-			});
-		}
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		console.log(initialValues);
-	};
 
 	return (
-		<form className="form-container" onSubmit={handleSubmit}>
+		<form className="form-container" onSubmit={formik.handleSubmit}>
 			<input
 				className="input-field"
 				type="text"
 				name="name"
-				value={initialValues.name}
-				onChange={handleChange}
+				value={formik.values.name}
+				onChange={formik.handleChange}
 				placeholder="Activity Title"
+				error={formik.errors.name}
 			/>
+			{formik.errors.name}
 			<input
 				className="input-field"
 				type="text"
 				name="description"
-				value={initialValues.description}
-				onChange={handleChange}
+				value={formik.values.description}
+				onChange={formik.handleChange}
 				placeholder="Write some activity description"
+				error={formik.errors.description}
 			/>
+			{formik.errors.description}
 			<label htmlFor="image">Upload Image:</label>
-			<input type="file" name="image" ref={imageInput} />
+			<input
+				type="file"
+				name="image"
+				onChange={e => {
+					formik.setFieldValue('image', e.target.files[0]);
+				}}
+				error={formik.errors.image}
+			/>
+			{formik.errors.image}
 			<button className="submit-btn" type="submit">
 				Send
 			</button>
