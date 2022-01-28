247a3a25487e16759a128b6c4744ca933a0d50c1
Martin Aja <martinjoseaja@gmail.com>
2022-01-28 18:13:47 -0300

    Add Spinner

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React from 'react';
-
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import LoginForm from './Components/Auth/LoginForm';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import LoginForm from "./Components/Auth/LoginForm";
 
 function App() {
   return (
-    <>
-    
-    <div className="App">
+     <div className="App">
       <header className="App-header">
-      <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/set-user" component={LoginForm} />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route path="/set-user" component={LoginForm} />
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
-    </>
   );
 }
 
--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,62 +1,90 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-const LoginForm = () => {
-const validateFields = (values) => {     // Checks user input is valid
-const errors = {}
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useState } from "react";
+import Spinner from "../Spinner";
 
- if (!values.email) {
-        errors.email = '* campo obligatorio'
+
+
+const LoginForm = () => {
+    
+  const validateFields = (values) => {
+    // Checks user input is valid
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = "* Campo obligatorio";
     }
     if (!values.password) {
-        errors.password = '* campo obligatorio'
+      errors.password = "* Campo obligatorio";
     }
     if (!values.confirmPassword) {
-        errors.confirmPassword = '* campo obligatorio'
+      errors.confirmPassword = "* Campo obligatorio";
     }
 
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = '* direccion de email incorrecta';
+      errors.email = "* Direccion de email incorrecta";
     }
-    if (values.password !== values.confirmPassword) (
-        errors.confirmPassword = '* las contraseñas deben coincidir'
-    )
+    if (values.password !== values.confirmPassword)
+      errors.confirmPassword = "* Las contraseñas deben coincidir";
     if (values.password.length < 6) {
-        errors.password = '* a longitud minima es 6'
+      errors.password = "* La longitud minima es 6";
     }
-    if (!/^(?=.*\d)(?=.*[a-zA-Záéíóúüñ])(?=.*[$-/:-?{-~!"^_`\]])/.test(values.password)) {
-        errors.password = '* la contraseña no cumple los parametros solicitados'           
+    if (
+      !/^(?=.*\d)(?=.*[a-zA-Záéíóúüñ])(?=.*[$-/:-?{-~!"^_`\]])/.test(
+        values.password
+      )
+    ) {
+      errors.password = "* La contraseña no cumple los parametros solicitados";
     }
 
-    return errors
-}
-const handleSubmit = (values) => {
+    return errors;
+    
+  };
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (values, loading) => {
     const userInfo = {
-        user: values.email,
-        password: values.password
-    }
-}
-    return (
-       
-        <Formik
-            initialValues={{ email: '', password: '', confirmPassword: '' }}
-            validate={validateFields}
-            onSubmit={(values) => handleSubmit(values)}
-        >
-            {({ isSubmitting }) => (
+      user: values.email,
+      password: values.password,
+    };
+  };
 
-                <Form className='form-container'>
-                    <Field type="email" name="email" className='input-field' placeholder="Ingrese email" />
-                    <ErrorMessage name="email" component="div" />
-                    <Field type="password" name="password" className='input-field' placeholder="Ingrese contraseña" />
-                    <ErrorMessage name="password" component="div" />
-                    <button type="submit-btn" className='submit-btn' disabled={isSubmitting}>
-                        Registrarse
-                    </button>
-                </Form>
-            )}
-        </Formik>
-    );
-
-            }
-export default LoginForm 
+  return (
+  
+    <Formik
+      initialValues={{ email: "", password: "", confirmPassword: "" }}
+      validate={validateFields}
+      onSubmit={(values) => handleSubmit(values)}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form-container">
+          <Field
+            type="email"
+            name="email"
+            className="input-field"
+            placeholder="Ingrese email"
+          />
+          <ErrorMessage name="email" component="div" />
+          <Field
+            type="password"
+            name="password"
+            className="input-field"
+            placeholder="Ingrese contraseña"
+          />
+          <ErrorMessage name="password" component="div" />
+          <button
+            type="submit-btn"
+            className="submit-btn"
+            disabled={isSubmitting}
+          >
+            Registrarse
+          </button>
+          <button onClick={()=>setLoading(true)}>Sarasa</button>
+          {
+              loading && <Spinner/>
+          }
+        </Form>
+      )}
+    </Formik>
+  );
+};
+export default LoginForm;
