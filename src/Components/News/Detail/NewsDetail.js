893ed46ee342af3f3e0a0fd0a4b10ea21fb3844e
Agustin Vargas <agustinvargas93@gmail.com>
2022-01-31 10:40:30 -0300

    Add functions to render a news content

--- src/Components/News/Detail/NewsDetail.js
+++ src/Components/News/Detail/NewsDetail.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container } from '@mui/material'
+import parse from 'html-react-parser'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import getANews from '../../../Services/getANews'
 
-export default function NewsDetail() {
-  return <div>NewsDetail</div>
+export default function NewsDetail({ title }) {
+  const { newsId } = useParams()
+  const [news, setNews] = useState(null)
+
+  useEffect(
+    () =>
+      (async () => {
+        const request = await getANews(newsId)
+        console.log('data', request)
+        const newsData = request?.data
+        setNews(newsData)
+      })(),
+    [newsId],
+  )
+
+  return (
+    <Container>
+      {title}
+      {news && parse(news.content)}
+      {news && <img src={news.image} alt={news.name} />}
+    </Container>
+  )
 }
