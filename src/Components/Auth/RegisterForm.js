1be6cb126761e6e7d22ac25438f8f7b6786ce642
LautaZap <86494089+LautaZap@users.noreply.github.com>
2022-02-01 20:48:05 -0300

    Ot128 60 - CustomCard component (#54)
    * Add files via upload (#6)
    
    * Add files via upload (#7)
    
    * add component card and html-react-parser
    
    * finish the card component
    
    * adjust on card component
    
    Co-authored-by: Martin Aja <98054747+martinaja@users.noreply.github.com>

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import '../FormStyles.css';
+import React from 'react'
+import '../FormStyles.css'
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const RegisterForm = () => {
+  const validateFields = (values) => {
+    // Checks user input is valid
 
-    const validateFields = (values) => {     // Checks user input is valid
+    const errors = {}
 
         const errors = {}
 
@@ -32,38 +34,68 @@ const RegisterForm = () => {
             errors.password = '* la contraseña no cumple los parametros solicitados'           
         }
 
-        return errors
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = '* direccion de email incorrecta'
+    }
+    if (values.password !== values.confirmPassword)
+      errors.confirmPassword = '* las contraseñas deben coincidir'
+    if (values.password.length < 6) {
+      errors.password = '* a longitud minima es 6'
+    }
+    if (
+      !/^(?=.*\d)(?=.*[a-zA-Záéíóúüñ])(?=.*[$-/:-?{-~!"^_`\[\]])/.test(
+        values.password,
+      )
+    ) {
+      errors.password = '* la contraseña no cumple los parametros solicitados'
     }
 
-    const handleSubmit = (values) => {
+    return errors
+  }
 
-        const userInfo = {
-            user: values.email,
-            password: values.password
-        }
+  const handleSubmit = (values) => {
+    const userInfo = {
+      user: values.email,
+      password: values.password,
     }
+  }
 
-    return (
-        <Formik
-            initialValues={{ email: '', password: '', confirmPassword: '' }}
-            validate={validateFields}
-            onSubmit={(values) => handleSubmit(values)}
-        >
-            {({ isSubmitting }) => (
-
-                <Form className='form-container'>
-                    <Field type="email" name="email" className='input-field' placeholder="Ingrese email" />
-                    <ErrorMessage name="email" component="div" />
-                    <Field type="password" name="password" className='input-field' placeholder="Ingrese contraseña" />
-                    <ErrorMessage name="password" component="div" />
-                    <Field type="password" name="confirmPassword" className='input-field' placeholder="Confirme su contraseña" />
-                    <ErrorMessage name="confirmPassword" component="div" />
-                    <button type="submit-btn" disabled={isSubmitting}>
-                        Registrarse
-                    </button>
-                </Form>
-            )}
-        </Formik>
-    );
+  return (
+    <Formik
+      initialValues={{ email: '', password: '', confirmPassword: '' }}
+      validate={validateFields}
+      onSubmit={(values) => handleSubmit(values)}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form-container">
+          <Field
+            type="email"
+            name="email"
+            className="input-field"
+            placeholder="Ingrese email"
+          />
+          <ErrorMessage name="email" component="div" />
+          <Field
+            type="password"
+            name="password"
+            className="input-field"
+            placeholder="Ingrese contraseña"
+          />
+          <ErrorMessage name="password" component="div" />
+          <Field
+            type="password"
+            name="confirmPassword"
+            className="input-field"
+            placeholder="Confirme su contraseña"
+          />
+          <ErrorMessage name="confirmPassword" component="div" />
+          <button type="submit-btn" disabled={isSubmitting}>
+            Registrarse
+          </button>
+        </Form>
+      )}
+    </Formik>
+  )
 }
+
 export default RegisterForm;
