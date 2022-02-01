1197f841583c4faf0254781e912a9c75ff903e80
LautaZap <lautarogzapata@gmail.com>
2022-02-01 15:08:18 -0300

    add Header for School campaign

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import LoginForm from "./Components/Auth/LoginForm";
-import OrganizationForm from "./Components/Organization/OrganizationForm";
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ActivitiesForm from './Components/Activities/ActivitiesForm'
+import CategoriesForm from './Components/Categories/CategoriesForm'
+import NewsForm from './Components/News/NewsForm'
+import SlidesForm from './Components/Slides/SlidesForm'
+import TestimonialForm from './Components/Testimonials/TestimonialsForm'
+import UserForm from './Components/Users/UsersForm'
+import SchoolCampaign from './Campaigns/School/SchoolCampaign'
+import ToysCampaign from './Campaigns/Toys/ToysCampaign'
+import MembersForm from './Components/Members/MembersForm'
+import ProjectsForm from './Components/Projects/ProjectsForm'
+import LoginForm from './Components/Auth/LoginForm'
+import OrganizationForm from './Components/Organization/OrganizationForm'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path="/create-activity" component={ActivitiesForm} />
@@ -47,7 +49,7 @@ function App() {
         </Switch>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
