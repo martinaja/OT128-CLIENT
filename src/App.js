8942b0eab8209263a4bc31334fb52132ad52b0b0
Agustín Vargas <82186687+agustinvargas@users.noreply.github.com>
2022-02-01 20:17:59 -0300

    Ot128 49 - Crear componente detalle de Novedad (#39)
    * Add files via upload (#6)
    
    * Add files via upload (#7)
    
    * Add files via upload (#8) (#9)
    
    * Add files via upload (#8) (#12)
    
    * Add files via upload
    
    * Add files via upload (#18)
    
    * Install html-react-parser
    
    * Add news detail route
    
    * Add news detail folder and component
    
    * Fix news detail route
    
    * delete sweet alert dependencies
    
    * Add functions to render a news content
    
    * Add custom styles
    
    * add loader
    
    Co-authored-by: Martin Aja <98054747+martinaja@users.noreply.github.com>
    Co-authored-by: LautaZap <86494089+LautaZap@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import LoginForm from "./Components/Auth/LoginForm";
-import OrganizationForm from "./Components/Organization/OrganizationForm";
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ActivitiesForm from './Components/Activities/ActivitiesForm'
+import CategoriesForm from './Components/Categories/CategoriesForm'
+import NewsForm from './Components/News/NewsForm'
+import SlidesForm from './Components/Slides/SlidesForm'
+import TestimonialForm from './Components/Testimonials/TestimonialsForm'
+import UserForm from './Components/Users/UsersForm'
+import SchoolCampaign from './Campaigns/School/SchoolCampaign'
+import ToysCampaign from './Campaigns/Toys/ToysCampaign'
+import MembersForm from './Components/Members/MembersForm'
+import ProjectsForm from './Components/Projects/ProjectsForm'
+import LoginForm from './Components/Auth/LoginForm'
+import NewsDetail from './Components/News/Detail/NewsDetail'
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route
-            exact
-            path="/backoffice/create-category/"
-            component={CategoriesForm}
-          />
-          <Route
-            path="/backoffice/create-category/:id"
-            component={CategoriesForm}
-          />
-          <Route exact path="/backoffice/news" component={NewsForm} />
-          <Route path="/backoffice/news/:newsId" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/set-user" component={LoginForm} />
-          <Route
-            path="/backoffice/organization/edit"
-            component={OrganizationForm}
-          />
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route exact path="/backoffice/news" component={NewsForm} />
+            <Route path="/backoffice/news/:newsId" component={NewsForm} />
+            <Route path="/novedades/:newsId" component={NewsDetail} />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route path="/set-user" component={LoginForm} />
+          </Switch>
+        </BrowserRouter>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
