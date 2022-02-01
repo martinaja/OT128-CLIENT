8907d97c8786d618775f53ff6a39d98b0eae1355
LautaZap <86494089+LautaZap@users.noreply.github.com>
2022-02-01 19:58:42 -0300

    Ot128 28 - categoryForm (#34)
    * add env variables for slides
    
    * fix a bug in delete and update
    
    * add formik and CKEditor to dependencies and create validateFields
    
    * add yup and CKEditor to categoriesForm component
    
    * add image selector
    
    * add petition get post and put for category endpoint
    
    * apply materialui to categoriesForm component
    
    * prueba
    
    * final commit of category form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
 import LoginForm from "./Components/Auth/LoginForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
       <BrowserRouter>
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path='/create-activity' component={ActivitiesForm} />
-          <Route path='/create-category' component={CategoriesForm} />
-          <Route exact path='/backoffice/news' component={NewsForm} />
-          <Route path='/backoffice/news/:newsId' component={NewsForm} />
-          <Route path='/backoffice/create-slide' component={SlidesForm} />
-          <Route path='/create-testimonials' component={TestimonialForm} />
-          <Route path='/create-user' component={UserForm} />
-          <Route path='/create-member' component={MembersForm} />
-          <Route path='/create-project' component={ProjectsForm} />
-          <Route path='/school-campaign' component={SchoolCampaign} />
-          <Route path='/toys-campaign' component={ToysCampaign} />
+          <Route path="/create-activity" component={ActivitiesForm} />
+          <Route
+            exact
+            path="/backoffice/create-category/"
+            component={CategoriesForm}
+          />
+          <Route
+            path="/backoffice/create-category/:id"
+            component={CategoriesForm}
+          />
+          <Route exact path="/backoffice/news" component={NewsForm} />
+          <Route path="/backoffice/news/:newsId" component={NewsForm} />
+          <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/create-testimonials" component={TestimonialForm} />
+          <Route path="/create-user" component={UserForm} />
+          <Route path="/create-member" component={MembersForm} />
+          <Route path="/create-project" component={ProjectsForm} />
+          <Route path="/school-campaign" component={SchoolCampaign} />
+          <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/set-user" component={LoginForm} />
         </Switch>
-        </BrowserRouter>
-        </header>
-    </div>
+      </BrowserRouter>
+    </>
   );
 }
 
