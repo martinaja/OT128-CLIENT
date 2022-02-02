664817439bc27460590d0232199af5a25eea9b9d
Agustin Vargas <agustinvargas93@gmail.com>
2022-02-02 13:15:11 -0300

    fix console err, remove title and news folder and comment code

--- src/Components/Home/ListNews.js
+++ src/Components/Home/ListNews.js
@@ -1,16 +1,13 @@
-import {  Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import React from 'react'
-import Title from '../../TitlesAndNews/Title'
-import NewList from "../News/NewsList"
+import NewList from '../News/NewsList'
 
 export const ListNews = () => {
   return (
     <div>
-   <Title/>
-   <NewList/>
-      <Typography>
-    Aquí irá la lista de novedades
-      </Typography>
+      {/* <Title/> */}
+      <NewList />
+      <Typography>Aquí irá la lista de novedades</Typography>
     </div>
   )
 }
