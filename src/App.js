5815ad93e857b0ffa7330d4d4b6b251c10238b1d
LautaZap <lautarogzapata@gmail.com>
2022-02-02 19:16:03 -0300

    commit of contact route

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import { Routes } from './Router/Routes'
-import ContactHome from './Components/Contact/ContactHome'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/contacto" component={ContactHome} />
-        </Switch>
-      </BrowserRouter>
     </div>
   )
 }
