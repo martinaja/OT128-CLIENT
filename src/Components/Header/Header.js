a1d90c492537755ddd7f8a849467f0be9c779b20
LautaZap <lautarogzapata@gmail.com>
2022-02-02 13:49:53 -0300

    commit de development a OT128-134

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -34,46 +34,17 @@ const Header = () => {
         }}
       >
         <Toolbar>
-          {isMatchTablet && (
-            <Box
-              component="img"
-              sx={{
-                width: 108,
-                height: 53,
-                m: 0,
-                p: 1,
-              }}
-              alt="Logo ong."
-              src="/images/Logo Somos Mas.png"
-            />
-          )}
-
           <Box
             component="img"
             sx={{
-              height: 60,
-              m: isMatchLaptop ? 0 : 'auto',
+              width: 108,
+              height: 53,
+              m: 'auto',
               p: 1,
             }}
-            alt="Logo campaña."
-            src="/images/estudio.png"
+            alt="Logo ong."
+            src="/images/Logo Somos Mas.png"
           />
-
-          {isMatchLaptop && (
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                flexGrow: 1,
-                display: { xs: 'none', sm: 'block' },
-                p: 1,
-                m: 'auto',
-              }}
-            >
-              Eslogan ejemplo.
-            </Typography>
-          )}
         </Toolbar>
       </AppBar>
     </Box>
