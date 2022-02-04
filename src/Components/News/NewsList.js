9f69505e09ffc89149cdc1eb32f38aa4bf4e28e9
Hernan <hernan.r9317@gmail.com>
2022-02-04 11:37:28 -0300

    SkeletonArticle added

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { GetPublicHandle } from '../../Services/publicApiService'
 import { Stack as Grid } from '@mui/material'
 import CustomCard from './../Card/CustomCard'
+import { SkeletonArticle } from './../Skeleton/SkeletonArticle'
 
 const NewsList = () => {
   const [data, setData] = useState('')
@@ -20,30 +21,36 @@ const NewsList = () => {
 
   return (
     <>
-      <h1>Novedades</h1>
-
-      <Grid
-        container="true"
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        direction={{ xs: 'column', md: 'row' }}
-      >
-        {data?.length > 0 ? (
-          data?.map((element) => {
-            return (
-              <Grid item="true" xs={2} sm={4} md={4} key={element.id}>
-                <CustomCard
-                  image={element.image}
-                  name={element.name}
-                  description={element.content}
-                />
-              </Grid>
-            )
-          })
-        ) : (
-          <p>No hay novedades</p>
-        )}
-      </Grid>
+      {!data ? (
+        <SkeletonArticle />
+      ) : (
+        <>
+          {' '}
+          <h1>Novedades</h1>
+          <Grid
+            container="true"
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+            direction={{ xs: 'column', md: 'row' }}
+          >
+            {data?.length > 0 ? (
+              data?.map((element) => {
+                return (
+                  <Grid item="true" xs={2} sm={4} md={4} key={element.id}>
+                    <CustomCard
+                      image={element.image}
+                      name={element.name}
+                      description={element.content}
+                    />
+                  </Grid>
+                )
+              })
+            ) : (
+              <p>No hay novedades</p>
+            )}
+          </Grid>
+        </>
+      )}
     </>
   )
 }
