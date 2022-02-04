f2c296ec833173e3d6388e8e926db59ffccd63e0
LautaZap <lautarogzapata@gmail.com>
2022-02-04 10:57:56 -0300

    add slider for toys campaign

--- src/Campaigns/Toys/Slider.js
+++ src/Campaigns/Toys/Slider.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-material-ui-carousel'
+import ItemSlider from '../ItemSlider'
 
 const Slider = () => {
+  const items = [
+    {
+      name: 'Imagen Campaña #1',
+      text: 'Texto al pie de la imagen ejemplo #1 campaña de juguetes',
+      image:
+        'https://drive.google.com/uc?export=view&id=11f7CNIvmomFoAAFOvnuI5gqnyHIB5b8x',
+    },
+    {
+      name: 'Imagen Campaña #2',
+      text: 'Texto al pie de la imagen ejemplo #2 campaña de juguetes',
+      image:
+        'https://drive.google.com/uc?export=view&id=1VogDd65eOHEIt-hyRpd-ysDf0SfTdWfs',
+    },
+    {
+      name: 'Imagen Campaña #3',
+      text: 'Texto al pie de la imagen ejemplo #3 campaña de juguetes',
+      image:
+        'https://drive.google.com/uc?export=view&id=1dgAJeb0r2GG-BIy3pink6mPda4KANZO6',
+    },
+  ]
+
   return (
-    <div>
-      
-    </div>
-  );
+    <Box sx={{ my: 2 }}>
+      <Carousel
+        interval="5000"
+        animation="slide"
+        duration="750"
+        swipe={false}
+        indicators={false}
+      >
+        {items.map((item, i) => (
+          <ItemSlider key={i} item={item} />
+        ))}
+      </Carousel>
+    </Box>
+  )
 }
- 
-export default Slider;
+
+export default Slider
