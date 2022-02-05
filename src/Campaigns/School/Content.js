fee3ae992beb22e51b85e98ccacce9330d2a0a75
Agustin Vargas <agustinvargas93@gmail.com>
2022-02-05 19:00:04 -0300

    Refactor school campaign content. Separate countdown component

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -1,26 +1,12 @@
-import {
-  Box,
-  Container,
-  createTheme,
-  Grid,
-  useMediaQuery,
-} from '@material-ui/core'
+import { Box, createTheme, Grid, Paper, useMediaQuery } from '@material-ui/core'
 import React from 'react'
 import Timer from '../../Components/Countdown/Timer'
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import PlaceIcon from '@mui/icons-material/Place'
+import './Content.css'
 
 const Content = () => {
-  // Breakpoints
-  // const theme = createTheme({
-  //   breakpoints: {
-  //     values: {
-  //       xs: 0,
-  //       sm: 450,
-  //       md: 600,
-  //       lg: 900,
-  //       xl: 1200,
-  //     },
-  //   },
-  // })
   const theme = createTheme({
     breakpoints: {
       values: {
@@ -35,7 +21,7 @@ const Content = () => {
 
   const isMatchTablet = useMediaQuery(theme.breakpoints.up('tablet'))
   const isMatchLaptop = useMediaQuery(theme.breakpoints.up('laptop'))
-  const isMatchDesktop = useMediaQuery(theme.breakpoints.up('desktop'))
+  // const isMatchDesktop = useMediaQuery(theme.breakpoints.up('desktop'))
   const isTV = useMediaQuery(theme.breakpoints.up('smartTv'))
 
   return (
@@ -44,15 +30,20 @@ const Content = () => {
       spacing={1}
       style={{ background: isTV && 'rgba(7, 34, 39, .1)' }}
     >
-      <Grid style={{ border: '1px dotted red' }} item xs={12}>
-        Fecha, hora, lugar del evento
+      <Grid item xs={12}>
+        <Paper elevation={5} className="date-container">
+          <div>
+            <CalendarTodayIcon /> <span>Fecha</span>
+          </div>
+          <div>
+            <AccessTimeIcon /> <span>Hora</span>
+          </div>
+          <div>
+            <PlaceIcon /> <span>Lugar</span>
+          </div>
+        </Paper>
       </Grid>
-      <Grid
-        style={{ border: '1px dotted red', padding: '20px' }}
-        item
-        xs={12}
-        lg={5}
-      >
+      <Grid style={{ padding: '20px' }} item xs={12} lg={5}>
         <main>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
@@ -83,105 +74,42 @@ const Content = () => {
         </main>
       </Grid>
       {isMatchLaptop && (
-        <Grid
-          style={{ border: '1px dotted red', padding: '20px' }}
-          item
-          xs={12}
-          lg={7}
-        >
+        <Grid item xs={12} lg={7}>
           {/* Hide on mobile & tablet */}
-          <section id="gallery">
-            <Box boxShadow={5} sx={{ p: 1 }} mb={4}>
-              <img width="100%" src="/images/blog-img-02.jpg" alt="" />
-            </Box>
-            <Box boxShadow={5} sx={{ p: 1 }}>
-              <img width="100%" src="/images/blog-img-02.jpg" alt="" />
-            </Box>
-            {/* <img width="100%" src="/images/blog-img-02.jpg" alt="" /> */}
+          <section id="gallery-campaign">
+            <Box
+              boxShadow={5}
+              style={{
+                height: '50vh',
+                width: '100%',
+                padding: '20px',
+                backgroundImage: 'url("/images/blog-img-02.jpg")',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+              }}
+            />
+            <Box
+              boxShadow={5}
+              style={{
+                height: '50vh',
+                width: '100%',
+                padding: '20px',
+                backgroundImage: 'url("/images/blog-img-02.jpg")',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+              }}
+            />
           </section>
         </Grid>
       )}
       {isMatchTablet && (
-        <Grid style={{ border: '1px dotted red' }} item xs={12}>
-          <Timer dateFromApi="2022-03-02T00:00:00.000000Z" />
+        <Grid item xs={12} style={{ padding: '50px 0' }}>
+          <Timer date="2022-03-02T00:00:00.000000Z" />
         </Grid>
       )}
     </Grid>
-    // <Container
-    // // style={{
-    // //   background: isMatchDesktop ? '' : 'rgb(7,34,39)',
-    // // }}
-    // >
-    //   <Box>
-    //     <main>
-    //       <p>
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-    //         tempor eu lacus quis scelerisque. Suspendisse placerat sed neque et
-    //         convallis. Fusce volutpat dui quis cursus finibus. Integer nulla
-    //         est, vestibulum vel dignissim et, tristique eu ante. Suspendisse
-    //         bibendum, nisl ut efficitur luctus, justo est imperdiet felis, ac
-    //         blandit nisl turpis vel purus. Nullam erat tortor, dictum in eros
-    //         ut, faucibus laoreet lorem. Aliquam tincidunt a sem et tristique.
-    //         Aliquam sollicitudin, turpis et congue scelerisque, tellus felis
-    //         molestie turpis, a ultrices risus arcu ac justo. Vestibulum non
-    //         tempor dui, sit amet bibendum nisi.
-    //         <p>
-    //           Vivamus pulvinar sapien sit amet nulla volutpat, eu finibus neque
-    //           dapibus. Nam at diam eros. Maecenas eleifend justo sed enim tempor
-    //           luctus. Donec vitae tempus metus. Cras dapibus semper tristique.
-    //           Vestibulum id enim in lectus dignissim rutrum ac sit amet ante.
-    //           Pellentesque pharetra eros neque, vel lobortis urna vulputate ac.
-    //           Integer quis sodales quam. Mauris dignissim urna et mi auctor
-    //           feugiat. Suspendisse feugiat pharetra quam rhoncus mattis. Nam et
-    //           mi gravida, rutrum diam eget, aliquam erat. Praesent sed finibus
-    //           sem, ut pellentesque tellus. Fusce placerat sit amet odio non
-    //           sollicitudin. Sed gravida ultrices purus placerat placerat.
-    //         </p>
-    //         <p>
-    //           Praesent viverra ligula a vehicula lobortis. Sed lectus turpis,
-    //           maximus sed tincidunt nec, vehicula non dolor. Suspendisse
-    //           dignissim urna sed velit porttitor tincidunt. Curabitur turpis
-    //           mauris, posuere nec mi at, suscipit elementum metus. Proin euismod
-    //           diam sed lacus dignissim, at feugiat eros tincidunt. Aenean
-    //           tincidunt, quam sit amet molestie viverra, arcu tortor tristique
-    //           tortor, quis blandit neque orci sed turpis. Aliquam tristique eget
-    //           risus et bibendum. Suspendisse dui dui, scelerisque non
-    //           condimentum vel, vulputate vel sapien. Sed egestas viverra quam,
-    //           vel vehicula ligula vehicula a. Lorem ipsum dolor sit amet,
-    //           consectetur adipiscing elit. Maecenas luctus mi et tempor pretium.
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    //         </p>
-    //         <p>
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et
-    //           neque et lectus iaculis laoreet. Quisque bibendum tristique
-    //           elementum. Praesent nec sapien dolor. Nullam porttitor pulvinar
-    //           luctus. Ut dapibus turpis ut eleifend gravida. Ut lorem dui,
-    //           feugiat sit amet nisl at, congue faucibus sem. Fusce lacinia
-    //           vulputate fringilla. Fusce hendrerit massa et mauris lacinia
-    //           pellentesque. Pellentesque leo nisi, rhoncus id sodales eu,
-    //           aliquet in felis.
-    //         </p>
-    //       </p>
-    //       <p>Fecha, hora y lugar</p>
-    //     </main>
-    //   </Box>
-    // {isMatchLaptop && (
-    //   <Box>
-    //     {/* Hide on mobile & tablet */}
-    //     <section id="gallery">
-    //       <p>Imágenes</p>
-    //     </section>
-    //   </Box>
-    // )}
-    //   {isMatchTablet && (
-    //     <Box>
-    //       {/* Hide on mobile */}
-    //       <section id="countdown">
-    //         <Timer dateFromApi="2022-03-02T00:00:00.000000Z" />
-    //       </section>
-    //     </Box>
-    //   )}
-    // </Container>
   )
 }
 
--- src/Components/Countdown/Timer.js
+++ src/Components/Countdown/Timer.js
@@ -1,70 +1,32 @@
 import React from 'react'
 import { Box, Paper } from '@material-ui/core'
 import Countdown, { zeroPad } from 'react-countdown'
+import './Timer.css'
 
-export default function Timer({ dateFromApi }) {
-  console.log('dateFromApi', dateFromApi)
-  const date = new Date(dateFromApi) /*Date from API*/
-  const dateToUTM = date.toUTCString()
+export default function Timer({ date }) {
+  const d = new Date(date)
+  const dateToUTM = d.toUTCString()
 
   const renderer = ({ days, hours, minutes, seconds }) => (
-    <div style={{ textAlign: 'center' }}>
-      <h2>La campaña comienza en...</h2>
-      <Box
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: '4em',
-          textAlign: 'center',
-          opacity: '0.9',
-        }}
-      >
-        <Paper
-          elevation={5}
-          style={{
-            border: '.1px solid black',
-            padding: '1.3em',
-            minWidth: '3em',
-          }}
-        >
-          <div style={{ fontSize: '4em' }}>{zeroPad(days)}</div>
+    <div className="timer-container">
+      <p className="timer-header">Comienza en...</p>
+      <Box className="timer-box">
+        <Paper elevation={5} className="timer-block-container">
+          <div className="timer-data">{zeroPad(days)}</div>
           <div>
             <small>días</small>
           </div>
         </Paper>
-        <Paper
-          elevation={3}
-          style={{
-            border: '.1px solid black',
-            padding: '1.3em',
-            minWidth: '3em',
-          }}
-        >
-          <div style={{ fontSize: '4em' }}>{zeroPad(hours)}</div>
+        <Paper elevation={3} className="timer-block-container">
+          <div className="timer-data">{zeroPad(hours)}</div>
           <small>hs</small>
         </Paper>
-        <Paper
-          elevation={3}
-          style={{
-            border: '.1px solid black',
-            padding: '1.3em',
-            minWidth: '3em',
-          }}
-        >
-          <div style={{ fontSize: '4em' }}>{zeroPad(minutes)}</div>
+        <Paper elevation={3} className="timer-block-container">
+          <div className="timer-data">{zeroPad(minutes)}</div>
           <small>min</small>
         </Paper>
-        <Paper
-          elevation={3}
-          style={{
-            border: '.1px solid black',
-            padding: '1.3em',
-            minWidth: '3em',
-          }}
-        >
-          <div style={{ fontSize: '4em' }}>{zeroPad(seconds)}</div>
+        <Paper elevation={3} className="timer-block-container">
+          <div className="timer-data">{zeroPad(seconds)}</div>
           <small>seg</small>
         </Paper>
       </Box>
