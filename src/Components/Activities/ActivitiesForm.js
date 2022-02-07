76ecd4237101d30c67ecbb4f970a12c4873966d1
Frank076-web <franco.776@hotmail.com>
2022-02-07 20:35:56 -0300

    Styled for activitiesForm

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,5 +1,5 @@
 import '../FormStyles.css'
-import { useFormik } from 'formik'
+import { ErrorMessage, Formik } from 'formik'
 import * as Yup from 'yup'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
@@ -13,6 +13,14 @@ import {
   putActivity,
 } from '../../Services/apiServices/activitiesApiService'
 import { alertServiceInfoTimer } from '../AlertService'
+import {
+  Box,
+  Button,
+  Container,
+  Input,
+  TextField,
+  Typography,
+} from '@mui/material'
 
 const ActivitiesForm = () => {
   let id = useParams().id
@@ -25,13 +33,17 @@ const ActivitiesForm = () => {
     image: undefined,
   }
   const formikValidationSchema = Yup.object({
-    name: Yup.string().required('Required'),
+    name: Yup.string().required('El campo nombre es obligatorio.'),
     description: Yup.string(),
     image: Yup.mixed()
-      .test('fileType', 'File must be an image jpg or png', (value) => {
-        if (value) return SUPPORTED_FORMATS.includes(value && value.type)
-      })
-      .required('Required'),
+      .required('ingrese una imagen')
+      .test(
+        'fileType',
+        'Formato incorrecto. Sólo se aceptan archivos .jpg, .jpeg, .png',
+        (value) => {
+          if (value) return SUPPORTED_FORMATS.includes(value && value.type)
+        },
+      ),
   })
 
   const handleSubmit = async (formData) => {
@@ -63,57 +75,84 @@ const ActivitiesForm = () => {
     console.log(responseServer)
   }, [responseServer])
 
-  const formik = useFormik({
-    initialValues: formikInitialValues,
-    validationSchema: formikValidationSchema,
-    onSubmit: handleSubmit,
-  })
-
   return (
-    <form className="form-container" onSubmit={formik.handleSubmit}>
-      <input
-        className="input-field"
-        type="text"
-        name="name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        placeholder="Activity Title"
-        error={formik.errors.name}
-      />
-      {formik.errors.name}
-      <CKEditor
-        editor={ClassicEditor}
-        data={formik.values.description}
-        onChange={(event, editor) => {
-          const data = editor.getData()
-          formik.setFieldValue('description', data)
-        }}
-      />
-      {formik.errors.description}
-      <label htmlFor="image">Upload Image:</label>
-      <input
-        type="file"
-        name="image"
-        onChange={(e) => {
-          formik.setFieldValue('image', e.target.files[0])
-        }}
-        error={formik.errors.image}
-      />
-      {formik.errors.image}
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-      {responseServer !== undefined
-        ? alertServiceInfoTimer(
-            'start',
-            'info',
-            responseServer.data.message,
-            false,
-            3000,
-          )
-        : null}
-      <LinearProgressFeedback isActive={isLoading} />
-    </form>
+    <Formik
+      enableReinitialize
+      initialValues={formikInitialValues}
+      validationSchema={formikValidationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        errors,
+        setFieldValue,
+        touched,
+      }) => (
+        <Container>
+          <Box sx={{ boxShadow: 5, p: 5, mt: 2 }}>
+            <Typography variant="h4">
+              {!id ? 'Crear Actividad' : 'Editar Actividad'}
+            </Typography>
+            <form className="form-container" onSubmit={handleSubmit}>
+              <TextField
+                margin="normal"
+                fullWidth
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={touched.name && errors.name}
+                label="Título Actividad"
+                error={touched.name && Boolean(errors.name)}
+              />
+              <CKEditor
+                editor={ClassicEditor}
+                data={values.description}
+                onChange={(event, editor) => {
+                  const data = editor.getData()
+                  setFieldValue('description', data)
+                }}
+              />
+              <ErrorMessage component="small" name="description" />
+              <label htmlFor="image">
+                <Input
+                  type="file"
+                  name="image"
+                  accept="image/*"
+                  id="image"
+                  style={{ display: 'none' }}
+                  onChange={(e) => {
+                    setFieldValue('image', e.target.files[0])
+                  }}
+                  error={errors.image}
+                />
+                <Button fullWidth variant="outlined" component="span">
+                  subir imagen
+                </Button>
+                <ErrorMessage component="small" name="image" />
+              </label>
+              <Button type="submit" variant="contained">
+                {id ? 'Editar actividad' : 'Crear actividad'}
+              </Button>
+              {responseServer !== undefined
+                ? alertServiceInfoTimer(
+                    'start',
+                    'info',
+                    responseServer.data.message,
+                    false,
+                    3000,
+                  )
+                : null}
+              <LinearProgressFeedback isActive={isLoading} />
+            </form>
+          </Box>
+        </Container>
+      )}
+    </Formik>
   )
 }
 
