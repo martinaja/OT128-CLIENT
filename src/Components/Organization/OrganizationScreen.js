2bd21d1fa393f61a118382f15a018a636f696be4
Frank076-web <franco.776@hotmail.com>
2022-02-08 18:28:34 -0300

    finalized organizationScreen component for backOffice

--- src/Components/Organization/OrganizationScreen.js
+++ src/Components/Organization/OrganizationScreen.js
@@ -1,44 +1,95 @@
 import {
-  Button,
+  Box,
   Card,
+  Button,
   CardActions,
   CardContent,
   CardMedia,
+  Container,
   Typography,
 } from '@mui/material'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { getOrganization } from '../../Services/apiServices/organizationApiService'
+import Spinner from '../Spinner'
 
 const OrganizationScreen = () => {
+  const [responseServer, setResponseServer] = useState(undefined)
+
   useEffect(() => {
     ;(async () => {
-      console.log(await getOrganization())
+      const response = await getOrganization()
+      setResponseServer(response.data)
     })()
-  })
+  }, [])
 
   return (
     <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      {responseServer ? (
+        <Container
+          sx={{
+            width: '100%',
+            height: '80vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Box sx={{ m: 2 }}>
+            <Card
+              sx={{
+                boxShadow: 5,
+                maxWidth: 800,
+                minHeight: 300,
+                marginRight: 'auto',
+                marginLeft: 'auto',
+              }}
+            >
+              <CardMedia
+                sx={{
+                  maxWidth: '30%',
+                  marginRight: 'auto',
+                  marginLeft: 'auto',
+                }}
+                component="img"
+                image={responseServer.data.logo}
+                alt={responseServer.data.name}
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h3"
+                  component="div"
+                  align="center"
+                >
+                  {responseServer.data.name}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  align="center"
+                >
+                  {responseServer.data.short_description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box>
+                  <Link
+                    style={{ textDecoration: 'none' }}
+                    to="/backoffice/organization/edit"
+                  >
+                    <Button variant="contained" color="success">
+                      Editar
+                    </Button>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        </Container>
+      ) : (
+        <Spinner />
+      )}
     </>
   )
 }
