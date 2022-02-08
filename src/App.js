7809c205fcaccb45b5f14d29937b270c1813bad9
Hernan <hernan.r9317@gmail.com>
2022-02-08 11:14:01 -0300

    ready to push

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from './Components/Header/Header'
 import { ThemeProvider } from '@mui/material/styles'
 import { createTheme, CssBaseline, Container } from '@mui/material'
 import { Routes } from './Router/Routes'
-import { MembersList } from './Components/Members/MembersList';
 
 const theme = createTheme({
   palette: {
@@ -15,10 +14,6 @@ const theme = createTheme({
 
 function App() {
   return (
-    <>
-      <MembersList />
-
-
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
@@ -26,9 +21,6 @@ function App() {
         <Routes />
       </Container>
     </ThemeProvider>
-
-
-    </>
   )
 }
 export default App
