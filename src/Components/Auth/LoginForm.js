4282ad68cb189765f8e4c805f29423612e99b8be
Martin Aja <martinjoseaja@gmail.com>
2022-02-08 19:43:37 -0300

    Create Edit Home

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,6 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import React, { useState } from "react";
-
+import '../FormStyles.css'
 
 const LoginForm = () => {
   const validateFields = (values) => {
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -25,7 +25,7 @@ function Footer() {
               src="/images/logo-bco.png"
               className="logo"
             />
-            <img alt="Logo ong." src="/images/oso-bco.png" className="logo" />
+            
           </Grid>
 
           <Grid item container md sm={12}>
