617ffa2373c5173ceca9ae3cc1cb514d2e0c2955
Hernan <hernan.r9317@gmail.com>
2022-02-08 10:44:04 -0300

    MembersList card added

--- src/Components/Members/MembersList.js
+++ src/Components/Members/MembersList.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { CardActions, Grid, Typography, Button, Card, CardMedia, CardContent } from '@mui/material';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { getPublicHandler } from './../../Services/BaseHTTP/publicApiService';
@@ -19,22 +19,55 @@ export const MembersList = () => {
 
   return <>
 
+
+
+
+  
+
         <h1>MemberLIst</h1>
         <Grid container rows={{ xs: 1, sm: 8, md: 6 }} spacing={{ xs: 2, md: 3 }}>
             {data?.length > 0 ? (
               data?.map((element) => {
                 return (
                   <Grid item key={element.id}>
-                    <CustomCard
-                      image={element.image}
-                      name={element.name}
-                      description={element.content}
-                    />
+
+
+                  <Card sx={{ maxWidth: 300}}>
+            <CardMedia
+                component="img"
+                image={element.image}
+                alt={element.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                <h4>{element.name}</h4>
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {element.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" href={element.facebookUrl}>Facebook</Button>
+                <Button size="small" href={element.linkedinUrl}>LinkedIn</Button>
+            </CardActions>
+            </Card>
+
+
+
+
+
+
+
+
+
+
+
+
                   </Grid>
                 )
               })
             ) : (
-              <p>No hay novedades</p>
+              <h1>No se encontraron resultados</h1>
             )}
           </Grid>
 
