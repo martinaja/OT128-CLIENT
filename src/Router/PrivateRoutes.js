0933f94b8267686fa1865403f7ee2ed55379bceb
Franco Pozzi <francojpozzi@gmail.com>
2022-02-09 20:42:30 -0300

    Modificacion rutas

--- src/Router/PrivateRoutes.js
+++ src/Router/PrivateRoutes.js
@@ -10,53 +10,24 @@ import OrganizationForm from '../Components/Organization/OrganizationForm'
 import BackOfficeHome from '../Components/BackOffice/BackOfficeHome'
 import OrganizationScreen from '../Components/Organization/OrganizationScreen'
 
-export const PrivateRoutes = () => {
-  return (
-    <>
-      <Route exact path="/backoffice" component={BackOfficeHome} />
-      <Route
-        exact
-        path="/backoffice/create-activity"
-        component={ActivitiesForm}
-      />
-      <Route
-        path="/backoffice/create-activity/:id"
-        component={ActivitiesForm}
-      />
-      <Route
-        exact
-        path="/backoffice/create-category"
-        component={CategoriesForm}
-      />
-      <Route
-        path="/backoffice/create-category/:id"
-        component={CategoriesForm}
-      />
-      <Route exact path="/backoffice/news" component={NewsForm} />
-      <Route path="/backoffice/news/:newsId" component={NewsForm} />
-      <Route path="/backoffice/members/edit" component={MembersForm} />
-      <Route path="/create-member" component={MembersForm} />
-      <Route exact path="/backoffice/slides" component={SlidesForm} />
-      <Route path="/backoffice/slides/:slideId" component={SlidesForm} />
-      <Route
-        exact
-        path="/backoffice/create-testimonials"
-        component={TestimonialForm}
-      />
-      <Route
-        path="/backoffice/create-testimonials/:id"
-        component={TestimonialForm}
-      />
-      <Route path="/create-project" component={ProjectsForm} />
-      <Route
-        exact
-        path="/backoffice/organization"
-        component={OrganizationScreen}
-      />
-      <Route
-        path="/backoffice/organization/edit"
-        component={OrganizationForm}
-      />
-    </>
-  )
-}
+export const privateRoutes = [
+  <Route path="/backoffice" component={BackOfficeHome} />,
+  <Route path="/backoffice/create-activity" component={ActivitiesForm} />,
+  <Route path="/backoffice/create-activity/:id" component={ActivitiesForm} />,
+  <Route path="/backoffice/create-category" component={CategoriesForm} />,
+  <Route path="/backoffice/create-category/:id" component={CategoriesForm} />,
+  <Route path="/backoffice/news" component={NewsForm} />,
+  <Route path="/backoffice/news/:newsId" component={NewsForm} />,
+  <Route path="/backoffice/members/edit" component={MembersForm} />,
+  <Route path="/create-member" component={MembersForm} />,
+  <Route path="/backoffice/slides" component={SlidesForm} />,
+  <Route path="/backoffice/slides/:slideId" component={SlidesForm} />,
+  <Route path="/backoffice/create-testimonials" component={TestimonialForm} />,
+  <Route
+    path="/backoffice/create-testimonials/:id"
+    component={TestimonialForm}
+  />,
+  <Route path="/create-project" component={ProjectsForm} />,
+  <Route path="/backoffice/organization" component={OrganizationScreen} />,
+  <Route path="/backoffice/organization/edit" component={OrganizationForm} />,
+]
