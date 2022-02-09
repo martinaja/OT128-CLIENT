8547b3b3f4e603d5a67bfde6211d18dbfb78421f
Franco Pozzi <francojpozzi@gmail.com>
2022-02-08 21:58:43 -0300

    Create Store and Auth Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counter from "../features/counter/counterSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authReducer'
 
-export default configureStore({
-  reducer: { counter },
-});
+const store = configureStore({
+  reducer: { auth: authReducer }, //add reducers
+})
+
+export default store
