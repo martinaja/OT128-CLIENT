7df1395f000bdd096b01e5f0d44510f10ffd8065
Franco Pozzi <francojpozzi@gmail.com>
2022-02-10 16:54:12 -0300

    Rutas Final

--- src/Router/PublicRoutes.js
+++ src/Router/PublicRoutes.js
@@ -11,4 +11,20 @@ import LoginForm from '../Components/Auth/LoginForm'
 import ContactHome from '../Components/Contact/ContactHome'
 import { Detail } from '../Components/Activities/Detail/Detail'
 
-export const publicRoutes = <></>
+export const publicRoutes = [
+  <Route exact path="/" component={Index} key="Index" />,
+  <Route path="/contacto" component={ContactHome} key="ContactHome" />,
+  <Route path="/novedades/:newsId" component={NewsDetail} key="NewsDetail" />,
+  <Route path="/nosotros" component={About} key="About" />,
+  <Route path="/create-user" component={UserForm} key="UserForm" />,
+  <Route
+    path="/school-campaign"
+    component={SchoolCampaign}
+    key="SchoolCampaign"
+  />,
+  <Route path="/toys-campaign" component={ToysCampaign} key="ToysCampaign" />,
+  <Route path="/login" component={LoginForm} key="LoginForm" />,
+  <Route path="/activities" component={Activities} key="Activities" />,
+  <Route path="/activities/:id" component={Detail} key="Detail" />,
+  <Route path="/news" component={NewsList} key="NewsList" />,
+]
