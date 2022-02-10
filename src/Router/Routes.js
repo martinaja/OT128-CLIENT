ebeffed7894fabdc29c654b8ef70df37a0daed47
Martin Aja <martinjoseaja@gmail.com>
2022-02-10 14:50:58 -0300

    Rotes changes

---
+++ src/Router/Routes.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from 'react-router-dom'
+
+import { PublicRoutes } from './PublicRoutes'
+import { PrivateRoutes } from './PrivateRoutes'
+
+export const Routes = () => {
+  return (
+    <Switch>
+      {PublicRoutes}
+      {PrivateRoutes}
+      <Route path={'*'} component={Error404} />
+    </Switch>
+  )
+}
+const Error404 = () => <h1>ERROR ERRROR</h1>
