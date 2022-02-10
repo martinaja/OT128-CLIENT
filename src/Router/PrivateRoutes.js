6ce1aff206d20f58381154b95759e508b25e623b
Martin Aja <martinjoseaja@gmail.com>
2022-02-10 14:54:34 -0300

    Changes routes

--- src/Router/PrivateRoutes.js
+++ src/Router/PrivateRoutes.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import ActivitiesForm from '../Components/Activities/ActivitiesForm'
 import CategoriesForm from '../Components/Categories/CategoriesForm'
 import NewsForm from '../Components/News/NewsForm'
@@ -7,66 +7,95 @@ import SlidesForm from '../Components/Slides/SlidesForm'
 import TestimonialForm from '../Components/Testimonials/TestimonialsForm'
 import ProjectsForm from '../Components/Projects/ProjectsForm'
 import OrganizationForm from '../Components/Organization/OrganizationForm'
+import UsersHome from '../Components/Users/UsersHome'
 import BackOfficeHome from '../Components/BackOffice/BackOfficeHome'
 import OrganizationScreen from '../Components/Organization/OrganizationScreen'
 import MembersScreen from '../Components/Members/MembersScreen'
 import ActivitiesScreen from '../Components/Activities/ActivitiesScreen'
 import UserForm from '../Components/Users/UsersForm'
-import SlidesScreen from './../Components/Slides/SlidesList';
+import SlidesScreen from './../Components/Slides/SlidesList'
+import { useSelector } from 'react-redux'
 
+
+function PrivateRoute({ children, ...rest }) {
+  const { isAuthenticated } = useSelector((state) => state.auth)
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return isAuthenticated === true ? children : <Redirect to="/login" />
+      }}
+    />
+  )
+}
 
 export const PrivateRoutes = () => {
   return (
     <>
-      <Route excat path="/backoffice" component={BackOfficeHome} />
-      <Route exact path="/backoffice/activities" component={ActivitiesScreen} />
-      <Route
-        exact
-        path="/backoffice/create-activity"
-        component={ActivitiesForm}
-      />
-      <Route
-        path="/backoffice/create-activity/:id"
-        component={ActivitiesForm}
-      />
-      <Route
-        exact
-        path="/backoffice/create-category"
-        component={CategoriesForm}
-      />
-      <Route
-        path="/backoffice/create-category/:id"
-        component={CategoriesForm}
-      />
-      <Route exact path="/backoffice/news" component={NewsForm} />
-      <Route path="/backoffice/news/:newsId" component={NewsForm} />
-      <Route exact path="/backoffice/members" component={MembersScreen} />
-      <Route path="/backoffice/members/edit" component={MembersForm} />
-      <Route path="/create-member" component={MembersForm} />
-      <Route exact path="/backoffice/slides" component={SlidesScreen} />
-      <Route exact path="/backoffice/slides/create" component={SlidesForm} />
-      <Route path="/backoffice/slides/create/:slideId" component={SlidesForm} />
-      <Route
-        exact
-        path="/backoffice/create-testimonials"
-        component={TestimonialForm}
-      />
-      <Route
-        path="/backoffice/create-testimonials/:id"
-        component={TestimonialForm}
-      />
-      <Route path="/create-project" component={ProjectsForm} />
-      <Route
-        exact
-        path="/backoffice/organization"
-        component={OrganizationScreen}
-      />
-      <Route
-        path="/backoffice/organization/edit"
-        component={OrganizationForm}
-      />
-      <Route exact path="/backoffice/users" component={UsersHome} />
-      <Route exact path="/backoffice/users/create" component={UserForm} />
+      <PrivateRoute path="/backoffice">
+        <BackOfficeHome />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/activities">
+        <ActivitiesScreen/>
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-activity">
+        <ActivitiesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-activity/:id">
+        <ActivitiesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-category">
+        <CategoriesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-category/:id">
+        <CategoriesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/news">
+        <NewsForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/news/:newsId">
+        <NewsForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/members">
+        <MembersScreen />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/members/edit">
+        <MembersForm />
+      </PrivateRoute>
+      <PrivateRoute path="/create-member">
+        <MembersForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/slides">
+        <SlidesScreen />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/slides/create">
+        <SlidesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/slides/create/:slideId">
+        <SlidesForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-testimonials">
+        <TestimonialForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/create-testimonials/:id">
+        <TestimonialForm />
+      </PrivateRoute>
+      <PrivateRoute path="/create-project">
+        <ProjectsForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/organization">
+        <OrganizationScreen />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/organization/edit">
+        <OrganizationForm />
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/users">
+        <UsersHome/>
+      </PrivateRoute>
+      <PrivateRoute path="/backoffice/users/create">
+        <UserForm />
+      </PrivateRoute>
     </>
   )
 }
