b51be56be0870936a342032e714ca07bf8fe2179
Franco Pozzi <francojpozzi@gmail.com>
2022-02-10 15:58:06 -0300

    cambio rutas

--- src/Router/PrivateRoutes.js
+++ src/Router/PrivateRoutes.js
@@ -1,37 +1,15 @@
-import { Route } from 'react-router-dom'
-import ActivitiesForm from '../Components/Activities/ActivitiesForm'
-import CategoriesForm from '../Components/Categories/CategoriesForm'
-import NewsForm from '../Components/News/NewsForm'
-import MembersForm from '../Components/Members/MembersForm'
-import SlidesForm from '../Components/Slides/SlidesForm'
-import TestimonialForm from '../Components/Testimonials/TestimonialsForm'
-import ProjectsForm from '../Components/Projects/ProjectsForm'
-import OrganizationForm from '../Components/Organization/OrganizationForm'
-import BackOfficeHome from '../Components/BackOffice/BackOfficeHome'
-import OrganizationScreen from '../Components/Organization/OrganizationScreen'
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-export const PrivateRoutes = (
-  <Route>
-    <Route path="/backoffice" component={BackOfficeHome} />,
-    <Route path="/backoffice/create-activity" component={ActivitiesForm} />,
-    <Route path="/backoffice/create-activity/:id" component={ActivitiesForm} />,
-    <Route path="/backoffice/create-category" component={CategoriesForm} />,
-    <Route path="/backoffice/create-category/:id" component={CategoriesForm} />,
-    <Route path="/backoffice/news" component={NewsForm} />,
-    <Route path="/backoffice/news/:newsId" component={NewsForm} />,
-    <Route path="/backoffice/members/edit" component={MembersForm} />,
-    <Route path="/create-member" component={MembersForm} />,
-    <Route path="/backoffice/slides" component={SlidesForm} />,
-    <Route path="/backoffice/slides/:slideId" component={SlidesForm} />,
-    <Route path="/backoffice/create-testimonials" component={TestimonialForm} />
-    ,
+export const PrivateRoute = ({ children, ...rest }) => {
+  const { isAuthenticated } = useSelector((state) => state.auth)
+
+  return (
     <Route
-      path="/backoffice/create-testimonials/:id"
-      component={TestimonialForm}
+      {...rest}
+      render={() => {
+        return isAuthenticated === true ? children : <Redirect to="/login" />
+      }}
     />
-    ,
-    <Route path="/create-project" component={ProjectsForm} />,
-    <Route path="/backoffice/organization" component={OrganizationScreen} />,
-    <Route path="/backoffice/organization/edit" component={OrganizationForm} />,
-  </Route>
-)
+  )
+}
--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,14 +1,115 @@
-import { privateRoutes } from './privateRoutes'
-import { publicRoutes } from './publicRoutes'
-import { Switch, Route } from 'react-router-dom'
+import { PrivateRoute } from './privateRoutes'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+import Index from '../Components/Home/Index'
+import NewsDetail from '../Components/News/Detail/NewsDetail'
+import About from '../Components/About/About'
+import Activities from '../Components/Activities/ActivitiesDetails'
+import NewsList from '../Components/News/NewsList'
+import UserForm from '../Components/Users/UsersForm'
+import SchoolCampaign from '../Campaigns/School/SchoolCampaign'
+import ToysCampaign from '../Campaigns/Toys/ToysCampaign'
+import LoginForm from '../Components/Auth/LoginForm'
+import ContactHome from '../Components/Contact/ContactHome'
+import { Detail } from '../Components/Activities/Detail/Detail'
+
+import ActivitiesForm from '../Components/Activities/ActivitiesForm'
+import CategoriesForm from '../Components/Categories/CategoriesForm'
+import NewsForm from '../Components/News/NewsForm'
+import MembersForm from '../Components/Members/MembersForm'
+import SlidesForm from '../Components/Slides/SlidesForm'
+import TestimonialForm from '../Components/Testimonials/TestimonialsForm'
+import ProjectsForm from '../Components/Projects/ProjectsForm'
+import OrganizationForm from '../Components/Organization/OrganizationForm'
+import UsersHome from '../Components/Users/UsersHome'
+import BackOfficeHome from '../Components/BackOffice/BackOfficeHome'
+import OrganizationScreen from '../Components/Organization/OrganizationScreen'
+import MembersScreen from '../Components/Members/MembersScreen'
+import ActivitiesScreen from '../Components/Activities/ActivitiesScreen'
+import SlidesScreen from './../Components/Slides/SlidesList'
 
 export const Routes = () => {
   return (
-    <Switch>
-      {publicRoutes}
-      {privateRoutes}
-      <Route path={'*'} component={Error404} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route path="/contacto" component={ContactHome} />
+        <Route path="/novedades/:newsId" component={NewsDetail} />
+        <Route path="/nosotros" component={About} />
+        <Route path="/create-user" component={UserForm} />
+        <Route path="/school-campaign" component={SchoolCampaign} />
+        <Route path="/toys-campaign" component={ToysCampaign} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/activities" component={Activities} />
+        <Route path="/activities/:id" component={Detail} />
+        <Route path="/news" component={NewsList} />
+        <PrivateRoute exact path="/backoffice">
+          <BackOfficeHome />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/activities">
+          <ActivitiesScreen />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-activity">
+          <ActivitiesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-activity/:id">
+          <ActivitiesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-category">
+          <CategoriesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-category/:id">
+          <CategoriesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/news">
+          <NewsForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/news/:newsId">
+          <NewsForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/members">
+          <MembersScreen />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/members/edit">
+          <MembersForm />
+        </PrivateRoute>
+        <PrivateRoute path="/create-member">
+          <MembersForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/slides">
+          <SlidesScreen />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/slides/create">
+          <SlidesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/slides/create/:slideId">
+          <SlidesForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-testimonials">
+          <TestimonialForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/create-testimonials/:id">
+          <TestimonialForm />
+        </PrivateRoute>
+        <PrivateRoute path="/create-project">
+          <ProjectsForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/organization">
+          <OrganizationScreen />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/organization/edit">
+          <OrganizationForm />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/users">
+          <UsersHome />
+        </PrivateRoute>
+        <PrivateRoute path="/backoffice/users/create">
+          <UserForm />
+        </PrivateRoute>
+
+        <Route path="*" component={Error404} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 const Error404 = () => <h1>ERROR ERRROR</h1>
