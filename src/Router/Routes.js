1f20bce33bf9786db65c27af670e5f44a0e9edbd
Franco Pozzi <81826599+franco-pozzi@users.noreply.github.com>
2022-02-10 17:01:08 -0300

    Refactor routes Final (#126)
    * Install html-react-parser
    
    * Add news detail route
    
    * Add news detail folder and component
    
    * Fix news detail route
    
    * delete sweet alert dependencies
    
    * Add functions to render a news content
    
    * Add custom styles
    
    * add loader
    
    * Add folder/component About and route it
    
    * Add api call
    
    * code clean up
    
    * code clean up
    
    * Add MembersForm route and install material icons
    
    * Add formik form and yup validation
    
    * Add preview image loaded config
    
    * Add slides routes
    
    * check order slides is alredy exists
    
    * fix console err, remove title and news folder and comment code
    
    * Add breakpoints and countdown
    
    * fix hour from countdown
    
    * fix countdown
    
    * Add a Timer component (countdown). Refactor the code
    
    * Add grid - layaout
    
    * Refactor school campaign content. Separate countdown component
    
    * Add content component - toys campaign
    
    * Fix api fetch. Add button loader
    
    * fix api fetch. add button loader
    
    * refactor
    
    * Fix backoffice route. Add main-header id (css). Hide main header on campaigns
    
    * Add API alerts on NewsForm component
    
    * Add Title component to NewsDetail
    
    * Add api error alert on NewsDetail
    
    * Add api error alerts on NewsList
    
    * Fix header display
    
    * Add Spinner loader in NewsForm
    
    * Add spinner in newsdetail
    
    * change grid media queries on campaign content
    
    * probando
    
    * Modificacion rutas
    
    * Modify PublicRoutes
    
    * Routes and PublicRoute
    
    * Test Routes Changes
    
    * cambio rutas
    
    * Rutas Final
    
    Co-authored-by: Agustin Vargas <agustinvargas93@gmail.com>
    Co-authored-by: Franco Pozzi <francojpozzi@gmail.com>
    Co-authored-by: LautaZap <86494089+LautaZap@users.noreply.github.com>

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,14 +1,17 @@
+import { Switch, Route } from 'react-router-dom'
 import { privateRoutes } from './privateRoutes'
 import { publicRoutes } from './publicRoutes'
-import { Switch, Route } from 'react-router-dom'
 
 export const Routes = () => {
   return (
-    <Switch>
-      {publicRoutes}
-      {privateRoutes}
-      <Route path={'*'} component={Error404} />
-    </Switch>
+    <>
+      <Switch>
+        {publicRoutes}
+        {privateRoutes}
+        <Route path="*" component={Error404} />
+      </Switch>
+    </>
+
   )
 }
 const Error404 = () => <h1>ERROR ERRROR</h1>
