67ab564f0075cde34bca131d754d0888395a2a8c
Frank076-web <franco.776@hotmail.com>
2022-02-11 19:06:49 -0300

    Added reducer into activitiesScreen and finished

--- src/features/activitiesReducer.js
+++ src/features/activitiesReducer.js
@@ -1,11 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { getActivity } from '../Services/apiServices/activitiesApiService'
 
-const initialState = {}
+const initialState = {
+  activities: [],
+}
 
 const activitiesSlice = createSlice({
   name: 'activities',
   initialState,
-  reducers: {},
+  reducers: {
+    getActivities: (state, action) => {
+      state.activities = action.payload
+    },
+  },
 })
 
+export const { getActivities } = activitiesSlice.actions
+
 export default activitiesSlice.reducer
+
+export const fetchActivities = () => async (dispatch) => {
+  const response = await getActivity()
+  dispatch(getActivities(response.data.data))
+}
