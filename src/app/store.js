9a37379c1341835d0346b9cb3c3cdc189785a682
Frank076-web <franco.776@hotmail.com>
2022-02-11 17:26:14 -0300

    created slice and put in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authReducer'
+import activitiesReducer from '../features/activitiesReducer'
 
 const store = configureStore({
-  reducer: { auth: authReducer }, //add reducers
+  reducer: { auth: authReducer, activities: activitiesReducer }, //add reducers
 })
 
 export default store
---
+++ src/features/activitiesReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+const activitiesSlice = createSlice({
+  name: 'activities',
+  initialState,
+  reducers: {},
+})
+
+export default activitiesSlice.reducer
