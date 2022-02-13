ab3e7649142502c09811fc217105384211dba6e1
LautaZap <lautarogzapata@gmail.com>
2022-02-12 21:52:28 -0300

    fix routes

--- src/Services/apiServices/usersApiService.js
+++ src/Services/apiServices/usersApiService.js
@@ -2,26 +2,26 @@ import {
   deletePrivateHandler,
   postPrivateHandler,
   putPrivateHandler,
+  getPrivateHandler,
 } from '../BaseHTTP/privateApiService'
-import { getPublicHandler } from '../BaseHTTP/publicApiService'
 
 const usersGetUrl = process.env.REACT_APP_API_USERS_GET
-const usersPostUrl = process.env.REACT_APP_API_USERS_POST
+const usersPostUrl = process.env.REACT_APP_API_USERS_POSTPrivate
 const usersPutUrl = process.env.REACT_APP_API_USERS_PUT
 const usersDeleteUrl = process.env.REACT_APP_API_USERS_DELETE
 
 export const getUsers = (id) => {
-  return getPublicHandler(testimonyGetUrl, id)
+  return getPrivateHandler(usersGetUrl, id)
 }
 
 export const postUsers = (id, bodydata) => {
-  return postPrivateHandler(testimonyPostUrl, id, bodydata)
+  return postPrivateHandler(usersPostUrl, id, bodydata)
 }
 
 export const putUsers = (id, bodydata) => {
-  return putPrivateHandler(testimonyPutUrl, id, bodydata)
+  return putPrivateHandler(usersPutUrl, id, bodydata)
 }
 
 export const deleteUsers = (id) => {
-  return deletePrivateHandler(testimonyDeleteUrl, id)
+  return deletePrivateHandler(usersDeleteUrl, id)
 }
