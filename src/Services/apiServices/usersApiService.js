2a2fea453e791aa2049a3023a43d17922d8a3f6f
LautaZap <lautarogzapata@gmail.com>
2022-02-14 14:49:25 -0300

    final commit

--- src/Services/apiServices/usersApiService.js
+++ src/Services/apiServices/usersApiService.js
@@ -3,14 +3,12 @@ import {
   postPrivateHandler,
   putPrivateHandler,
   getPrivateHandler,
-  searchPrivateHandler,
 } from '../BaseHTTP/privateApiService'
 
 const usersGetUrl = process.env.REACT_APP_API_USERS_GET
 const usersPostUrl = process.env.REACT_APP_API_USERS_POSTPrivate
 const usersPutUrl = process.env.REACT_APP_API_USERS_PUT
 const usersDeleteUrl = process.env.REACT_APP_API_USERS_DELETE
-const userSearchUrl = process.env.REACT_APP_API_USERS_SEARCH
 
 export const getUsers = (id) => {
   return getPrivateHandler(usersGetUrl, id)
@@ -30,6 +28,6 @@ export const deleteUsers = (id) => {
 
 export const searchUsers = ({ name, type }) => {
   let searchParams
-  searchParams = `?search=${name}&role=${type}`
-  return searchPrivateHandler(userSearchUrl, searchParams)
+  searchParams = `${usersGetUrl}?search=${name}&role=${type}`
+  return getPrivateHandler(searchParams)
 }
