ad9724b441d6e758a95fd4c49a22bec49a78cbda
Franco Pozzi <francojpozzi@gmail.com>
2022-02-15 17:02:43 -0300

    add redirect

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Formik, Field, Form } from 'formik';
-import * as Yup from 'yup';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Avatar from '@material-ui/core/Avatar';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import { Link as RouterLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { userLogin } from './../../features/auth/authReducer';
+import React from 'react'
+import { Formik, Field, Form } from 'formik'
+import * as Yup from 'yup'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Avatar from '@material-ui/core/Avatar'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import { Link as RouterLink, Redirect } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogin } from './../../features/auth/authReducer'
 
-const useStyles = makeStyles(theme => ({
+export default function LoginForm() {
+  const authData = useSelector(({ auth }) => auth)
+
+  if (authData.isAuthenticated) {
+    return <Redirect push to="/backoffice" />
+  }
+
+  return <FormLogic authData={authData} />
+}
+
+const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -29,103 +39,109 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+}))
 
-export default function LoginForm() {
-  const classes = useStyles();
-const dispatch = useDispatch();
+const FormLogic = ({ authData }) => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
 
   return (
-    <Container component='main' maxWidth='xs' style={{
-    
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '2rem',
-    }}>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Ingresar
-        </Typography>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-            rememberUser: false
-          }}
-          validationSchema={Yup.object({
-            email: Yup.string()
-              .email('Invalid email address')
-              .required('Required'),
-            password: Yup.string()
-              .required('No password provided.')
-          })}
-          onSubmit={(values) => {
-            const {email, password} = values;
-         dispatch(userLogin({email, password}));
-          }}>
+    <>
+      <Container
+        component="main"
+        maxWidth="xs"
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '2rem',
+        }}
+      >
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Ingresar
+          </Typography>
+          <Formik
+            initialValues={{
+              email: '',
+              password: '',
+              rememberUser: false,
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
+              password: Yup.string().required('No password provided.'),
+            })}
+            onSubmit={(values) => {
+              const { email, password } = values
 
-          <Form>
-            <Field name='email'>
-              {({ field, form, meta }) => (
-                <TextField
-                  variant='outlined'
-                  margin='normal'
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  autoComplete='email'
-                  autoFocus
-                  inputProps = {{...field}}
-                  error = {meta.touched && typeof meta.error !== 'undefined'}
-                />
-              )}
-            </Field>
-            <Field name='password' type='password'>
-              {({ field, form, meta }) => (
-                <TextField
-                  variant='outlined'
-                  margin='normal'
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  autoComplete='current-password'
-                  inputProps = {{...field}}
-                  error = {meta.touched && typeof meta.error !== 'undefined'}
-                />
-              )}
-            </Field>
-         
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              color='primary'
-              className={classes.submit}
-            >
-              Ingresar
-            </Button>
+              const data = { email: email, password: password }
+
+              dispatch(userLogin(data))
+            }}
+          >
+            <Form>
+              <Field name="email">
+                {({ field, form, meta }) => (
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    inputProps={{ ...field }}
+                    error={meta.touched && typeof meta.error !== 'undefined'}
+                  />
+                )}
+              </Field>
+              <Field name="password" type="password">
+                {({ field, form, meta }) => (
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    inputProps={{ ...field }}
+                    error={meta.touched && typeof meta.error !== 'undefined'}
+                  />
+                )}
+              </Field>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Ingresar
+              </Button>
               <Grid container>
                 <Grid item>
-                  <Link component={RouterLink} to='/home/register'>
+                  <Link component={RouterLink} to="/home/register">
                     {'No tiene una cuenta? Registrese'}
                   </Link>
                 </Grid>
               </Grid>
-          </Form>
-        </Formik>
-      </div>
-    </Container>
-  );
+            </Form>
+          </Formik>
+        </div>
+      </Container>
+    </>
+  )
 }
-
--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Link from '@material-ui/core/Link';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import { Link as RouterLink } from 'react-router-dom';
-import { FormTextInput as TextInput} from './VisualComponents/Inputs';
-import { userRegister } from './../../features/auth/authReducer';
-import { useDispatch } from 'react-redux';
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Link from '@material-ui/core/Link'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import { Link as RouterLink, Redirect } from 'react-router-dom'
+import { FormTextInput as TextInput } from './VisualComponents/Inputs'
+import { userRegister } from './../../features/auth/authReducer'
+import { useDispatch, useSelector } from 'react-redux'
 
+export default function RegisterForm() {
+  const authData = useSelector(({ auth }) => auth)
+
+  if (authData.isAuthenticated) {
+    return <Redirect push to="/backoffice" />
+  }
+
+  return <FormLogic authData={authData} />
+}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -33,22 +42,23 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+}))
 
-export default function RegisterForm() {
-  const classes = useStyles();
-  const dispatch = useDispatch();
+const FormLogic = () => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
 
   return (
-    <Container component="main" maxWidth="xs" 
-    style={{
-    
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '2rem',
-  }}
+    <Container
+      component="main"
+      maxWidth="xs"
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '2rem',
+      }}
     >
       <CssBaseline />
       <div className={classes.paper}>
@@ -61,76 +71,78 @@ export default function RegisterForm() {
         <Formik
           initialValues={{
             name: '',
-         email: '',
+            email: '',
             password: '',
-            mailerConsent: false
+            mailerConsent: false,
           }}
           validationSchema={Yup.object({
-            name: Yup.string()
-              .required('Required'),
-           
-           email: Yup.string()
+            name: Yup.string().required('Required'),
+
+            email: Yup.string()
               .email('Invalid email address')
               .required('Required'),
             password: Yup.string()
               .required('Required')
-              .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&.*])(?=.{8,})/, "Password must contain at least one upper case character, one lower case character, one number, and must be at least 8 characters long")
+              .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&.*])(?=.{8,})/,
+                'Password must contain at least one upper case character, one lower case character, one number, and must be at least 8 characters long',
+              ),
           })}
           onSubmit={(values) => {
-            const {name, email, password} = values;
-        
-   dispatch(userRegister({name, email, password}));
-          }}>
-         
+            const { name, email, password } = values
+
+            dispatch(userRegister({ name, email, password }))
+          }}
+        >
           <Form className={classes.form} noValidate>
             <TextInput
-              variant='outlined'
-              margin='normal'
+              variant="outlined"
+              margin="normal"
               required
               fullWidth
-              id='name'
-              label='name'
-              name='name'
-              autoComplete='name'
+              id="name"
+              label="name"
+              name="name"
+              autoComplete="name"
               autoFocus
             />
-           
+
             <TextInput
-              variant='outlined'
-              margin='normal'
+              variant="outlined"
+              margin="normal"
               required
               fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
             />
             <TextInput
-              variant='outlined'
-              margin='normal'
+              variant="outlined"
+              margin="normal"
               required
               fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
             />
-         
+
             <Button
-              type='submit'
+              type="submit"
               fullWidth
-              variant='contained'
-              color='primary'
+              variant="contained"
+              color="primary"
               className={classes.submit}
             >
               Sign Up
             </Button>
           </Form>
         </Formik>
-        <Link component={RouterLink} to='/login'>
+        <Link component={RouterLink} to="/login">
           {'Ya tiene una cuenta? Ingrese'}
         </Link>
       </div>
     </Container>
-  );
+  )
 }
