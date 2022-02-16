d75875ccc23ee6fee7ec0ac10b6f113e7d042634
Martin Aja <martinjoseaja@gmail.com>
2022-02-16 16:19:38 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -10,20 +10,7 @@ const theme = createTheme({
       default: 'rgb(218 246 247)',
       m: 0,
     },
-    palette: {
-      primary: {
-        light: '#757ce8',
-        main: '#3f50b5',
-        dark: '#002884',
-        contrastText: '#fff',
-      },
-      secondary: {
-        light: '#ff7961',
-        main: '#f44336',
-        dark: '#ba000d',
-        contrastText: '#000',
-      },
-    },
+
   },
 })
 
