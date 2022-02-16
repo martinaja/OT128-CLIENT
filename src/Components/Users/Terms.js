e89b2e546ba5f9c4e753568c0bfdb1d03851db3c
Hernan <hernan.r9317@gmail.com>
2022-02-16 12:52:19 -0300

    upadated3

--- src/Components/Users/Terms.js
+++ src/Components/Users/Terms.js
@@ -8,10 +8,9 @@ export const Terms = () => {
   const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
 
-  const onDocumentLoadSuccess = ({ numPages })  => {
+  const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages)
   }
-
 
   return (
     <>
