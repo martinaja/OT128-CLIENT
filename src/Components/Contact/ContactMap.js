6cc4853b10090cb286a0853a3208edef9343f966
LautaZap <lautarogzapata@gmail.com>
2022-02-16 11:59:54 -0300

    added data from api to the map

--- src/Components/Contact/ContactMap.js
+++ src/Components/Contact/ContactMap.js
@@ -1,18 +1,26 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import './ContactMap.css'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Icon } from 'leaflet'
 import * as ELG from 'esri-leaflet-geocoder'
+import { useDispatch, useSelector } from 'react-redux'
+import { alertServiceError } from '../AlertService'
+import { getOrganizationData } from '../../features/organization/organizationReducer'
+import Spinner from '../Spinner'
 
 const ContactMap = () => {
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state.organization)
+
   const [showInfo, setShowInfo] = useState(false)
   const [geo, setGeo] = useState({})
 
+  // icon that will indicate the location
   const icon = new Icon({
-    iconUrl: '/images/Logo Somos Mas.png',
-    iconSize: [75, 40],
+    iconUrl: '/images/marker somos mas.png',
+    iconSize: [55, 70],
   })
 
+  // onCLick event for the marker
   const eventHandlers = useMemo(
     () => ({
       click() {
@@ -23,23 +31,42 @@ const ContactMap = () => {
   )
 
   useEffect(() => {
-    if (!geo) return
-    ELG.geocode({ apikey: process.env.REACT_APP_GEOCODE_API_KEY })
-      .text('Las garzas 2609, Paraná ,Entre ríos')
-      .run(function (err, results, response) {
-        if (err) {
-          console.log(err)
-          return
-        }
-        setGeo(response.candidates[0].location)
-      })
-  }, [])
+    if (state.status === 'idle') {
+      dispatch(getOrganizationData())
+    }
 
+    if (state.status === 'error') {
+      alertServiceError(
+        state.errorMsg,
+        'Se produjo un error al intentar obtener datos de la organización',
+      )
+    }
+    console.log(state)
+  }, [state.status, dispatch, state.errorMsg])
+
+  // Transform the string of adress in latitud and longitud for mapcointaner
+  useEffect(() => {
+    if (!geo) return
+    if (state.status === 'success') {
+      console.log('asdasd')
+      ELG.geocode({ apikey: process.env.REACT_APP_GEOCODE_API_KEY })
+        .text(state.data.address)
+        .run(function (err, results, response) {
+          if (err) {
+            console.log(err)
+            return
+          }
+          setGeo(response.candidates[0].location)
+        })
+    }
+  }, [state.status])
+
+  // if geo.y exist render MapContainer, otherwise the app will crash
   return geo.y ? (
     <MapContainer
       center={[geo.y, geo.x]}
-      zoom={14}
-      style={{ width: '80%', height: '100vh' }}
+      zoom={20}
+      style={{ width: '100%', height: '100vh' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -59,11 +86,14 @@ const ContactMap = () => {
             setShowInfo(false)
           }}
         >
-          Hola
+          <h2>{state.data.name}</h2>
+          <p>{state.data.short_description}</p>
         </Popup>
       )}
     </MapContainer>
-  ) : null
+  ) : (
+    <Spinner />
+  )
 }
 
 export default ContactMap
--- src/features/organization/organizationReducer.js
+++ src/features/organization/organizationReducer.js
@@ -1,30 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { getOrganization } from '../../Services/apiServices/organizationApiService'
 
 const initialState = {
   data: {},
-  status: '',
+  status: 'idle',
+  errorMsg: '',
 }
 
 //Export to RegisterForm submitHandle => catch Error
 export const getOrganizationData = createAsyncThunk(
   'organization/getOrganizationData',
-  async (data, { rejectWithValue }) => {
+  async () => {
     try {
-      const response = await axios.get(
-        'http://ongapi.alkemy.org/api/organizations',
-      )
+      const response = await getOrganization()
       return response.data
-    } catch {
-      return rejectWithValue({ error: 'error' })
+    } catch (err) {
+      throw new Error(err)
     }
   },
 )
 
-export const organizationReducer = createSlice({
+export const organizationSlice = createSlice({
   name: 'organization',
   initialState,
-  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getOrganizationData.pending, (state) => {
@@ -32,12 +30,13 @@ export const organizationReducer = createSlice({
       })
       .addCase(getOrganizationData.fulfilled, (state, action) => {
         state.data = action.payload.data
-        state.status = action.payload.success
+        state.status = 'success'
       })
       .addCase(getOrganizationData.rejected, (state, action) => {
-        state.status = action.payload.error
+        state.errorMsg = action.payload.error
+        state.status = 'error'
       })
   },
 })
 
-export default organizationReducer.reducer
+export default organizationSlice.reducer
