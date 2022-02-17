259dc8275e82a95375ff0df23057f1e9dbfec9fc
LautaZap <86494089+LautaZap@users.noreply.github.com>
2022-02-17 15:20:11 -0300

    Ot128 129 - Optimizar ruteo con lazy loading (#178)
    * isntall react-leaflet and config map with mock data
    
    * add geocode to transform string to latitude and longitud and one api key in .env
    
    * added data from api to the map
    
    * final commit
    
    * add home for news in public routes and video section
    
    * refactor of newsDetail NewsSearch and PublicNewHome
    
    * refactor
    
    * final commit
    
    * add lazy load to public routes
    
    * add Loading page for fallback in Suspense component
    
    * bug fixes in 404 page

--- src/Components/Donations/Thankyou.js
+++ src/Components/Donations/Thankyou.js
@@ -1,14 +1,16 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-export const Thankyou = () => {
+const Thankyou = () => {
   return (
     <>
-    <Box  sx={{ m: 2 }}>
-      <Typography variant="h4" component="div" gutterBottom>
-        Muchas gracias por su donación!
-      </Typography>
+      <Box sx={{ m: 2 }}>
+        <Typography variant="h4" component="div" gutterBottom>
+          Muchas gracias por su donación!
+        </Typography>
       </Box>
     </>
   )
 }
+
+export default Thankyou
