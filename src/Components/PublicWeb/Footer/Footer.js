153865acb719cb30464acc675a94d0fe09d22c2c
Martin Aja <martinjoseaja@gmail.com>
2022-02-18 19:45:20 -0300

    Footer

--- src/Components/PublicWeb/Footer/Footer.js
+++ src/Components/PublicWeb/Footer/Footer.js
@@ -1,7 +1,8 @@
-import { Grid } from '@material-ui/core'
+import {  Box, Grid } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Footer.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLinkedin,
@@ -14,60 +15,67 @@ function Footer() {
   return (
     <>
       <div className="container">
-        <Grid container className="footer__container">
-          <Grid
-            item
-            container
-            md={4}
-            sm={12}
-            alignItems="center"
-            className="center"
-          >
-            <img alt="Logo ong." src="/images/logo-bco.png" className="logo" />
-          </Grid>
+        
+          <Grid container className="footer__container">
+            <Grid container alignItems="center" className="center">
+              <Link to={'/'}>
+                <img
+                  alt="Logo ong."
+                  src="/images/logo-bco.png"
+                  className="logo"
+                />
+              </Link>
+          <Box style={{display:"flex"}}>
+         <Link to={'/news'} className="col" >
+              <p className="col__title">Novedades</p>
+              </Link>
+              <Link to={'/activities'} className="col">
+                <p className="col__title">Actividades</p>
+              </Link>
+              <Link to={'/nosotros'} className="col">
+                <p className="col__title">Nosotros</p>
+              </Link>
+              <Link to={'/testimonios'} className="col">
+                <p className="col__title">Testimonios </p>
+              </Link>
+              <Link to={'/donations'} className="col">
+                <p className="col__title">Contribuye</p>
+              </Link>
+              <Link to={'/contacto'} className="col" xs={12}>
+                <p className="col__title">Contacto</p>
+              </Link>
+         </Box>
+              
+            </Grid>
 
-          <Grid item container md sm={12}>
-            <Link to={'/news'} item className="col" xs={12} sm md>
-              <div className="col__title">Novedades</div>
-            </Link>
-            <Link to={'/activities'} item className="col" xs={12} sm md>
-              <div className="col__title">Actividades</div>
-            </Link>
-            <Link to={'/nosotros'} item className="col" xs={12} sm md>
-              <div className="col__title">Nosotros</div>
-            </Link>
-            <Link to={'/testimonios'} item className="col" xs={12} sm md>
-              <div className="col__title">Testimonios </div>
-            </Link>
-            <Link to={'/donations'} item className="col" xs={12} sm md>
-              <div className="col__title">Contribuye</div>
-            </Link>
-            <Link to={'/contacto'} item className="col" xs={12} sm md>
-              <div className="col__title">Contacto</div>
-            </Link>
-          </Grid>
+       
+            
 
-          <Grid item md={12} sm={12} className="social">
-            <hr className="social__hr" />
-            <div className="social__tags">
-              <a href="https://www.facebook.com" className="facebook social">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-              <a href="https://www.twitter.com" className="twitter social">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </a>
-              <a href="https://www.instagram.com" className="instagram social">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-              <a href="https://www.linkedin.com" className="linkedin social">
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-              </a>
-            </div>
-            <div className="social__copyrights">
-              © 2020 Somos mas. All Rights Reserved.
-            </div>
+            <Grid className="social">
+              <hr className="social__hr" />
+              <div className="social__tags">
+                <a href="https://www.facebook.com" className="facebook social">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.twitter.com" className="twitter social">
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a
+                  href="https://www.instagram.com"
+                  className="instagram social"
+                >
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a href="https://www.linkedin.com" className="linkedin social">
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+              </div>
+              <div className="social__copyrights">
+                © 2020 Somos mas. All Rights Reserved.
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+    
       </div>
     </>
   )
