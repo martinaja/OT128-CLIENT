bb490b3528bf31a97bf6a62e428dc31a2e096852
Martin Aja <martinjoseaja@gmail.com>
2022-02-18 19:15:34 -0300

    Ot128 144 - Restrict access to Login form for authenticated user (#176)
    * Install html-react-parser
    
    * Add news detail route
    
    * Add news detail folder and component
    
    * Fix news detail route
    
    * delete sweet alert dependencies
    
    * Add functions to render a news content
    
    * Add custom styles
    
    * add loader
    
    * Add folder/component About and route it
    
    * Add api call
    
    * code clean up
    
    * code clean up
    
    * Add MembersForm route and install material icons
    
    * Add formik form and yup validation
    
    * Add preview image loaded config
    
    * Add slides routes
    
    * check order slides is alredy exists
    
    * fix console err, remove title and news folder and comment code
    
    * Add breakpoints and countdown
    
    * fix hour from countdown
    
    * fix countdown
    
    * Add a Timer component (countdown). Refactor the code
    
    * Add grid - layaout
    
    * Refactor school campaign content. Separate countdown component
    
    * Add content component - toys campaign
    
    * Fix api fetch. Add button loader
    
    * fix api fetch. add button loader
    
    * refactor
    
    * Fix backoffice route. Add main-header id (css). Hide main header on campaigns
    
    * Add API alerts on NewsForm component
    
    * Add Title component to NewsDetail
    
    * Add api error alert on NewsDetail
    
    * Add api error alerts on NewsList
    
    * Fix header display
    
    * Add Spinner loader in NewsForm
    
    * Add spinner in newsdetail
    
    * change grid media queries on campaign content
    
    * probando
    
    * Modificacion rutas
    
    * Modify PublicRoutes
    
    * Routes and PublicRoute
    
    * Test Routes Changes
    
    * Rotes changes
    
    * cambio rutas
    
    * Rutas Final
    
    * Refactor routes
    
    * Protect private paths
    
    * Protect private paths
    
    * Create News search form (Public Web)
    
    * Add loading and alerts in Home and About us
    
    * Reducer news
    
    * Create Slides Reducer
    
    * Create Slides Reducer
    
    * Create Slides Reducer
    
    * Create Slides Reducer
    
    * Create Slides search form (Backoffice)
    
    * Changes routes
    
    * Apply api ti login/register
    
    * Changes Register
    
    * Login register interaction api
    
    * add redirect
    
    * Changes login and reducer
    
    * Integrate Mercado Pago on screen "Donation"
    
    * Prueba
    
    * Resolved conlicts
    
    * Changes
    
    * Routes
    
    * Styles
    
    * Fixed
    
    * Router solutions
    
    * Fixed 404
    
    * Restrict access to Login form for authenticated user
    
    * Header
    
    * Edit header and add missing links
    
    Co-authored-by: Agustin Vargas <agustinvargas93@gmail.com>
    Co-authored-by: Franco Pozzi <francojpozzi@gmail.com>
    Co-authored-by: LautaZap <86494089+LautaZap@users.noreply.github.com>
    Co-authored-by: Agustín Vargas <82186687+agustinvargas@users.noreply.github.com>
    Co-authored-by: Frank076-web <franco.776@hotmail.com>
    Co-authored-by: Franco Pozzi <81826599+franco-pozzi@users.noreply.github.com>

--- src/Components/PublicWeb/Header/Header.js
+++ src/Components/PublicWeb/Header/Header.js
@@ -2,69 +2,97 @@ import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import styles from './Header.module.css'
-import logo from './logo-bco.png'
+import logo from './logo-letras-blancas.png'
 import { arrayData } from './data'
-
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogout } from '../../../features/auth/authReducer'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
   const [isOpen, setIsOpen] = useState(false)
-
   const toggle = () => setIsOpen(!isOpen)
-
   let { pathname = '' } = useLocation()
-
   const userAuth = useSelector((state) => state.auth.isAuthenticated)
 
   return (
     <div id="main-header">
-      <NavBar toggle={toggle} pathname={pathname} userAuth={userAuth} />
-      <SideBar toggle={toggle} isOpen={isOpen} userAuth={userAuth} />
+      <NavBar
+        toggle={toggle}
+        pathname={pathname}
+        userAuth={userAuth}
+        dispatch={dispatch}
+      />
+      <SideBar
+        toggle={toggle}
+        isOpen={isOpen}
+        userAuth={userAuth}
+        dispatch={dispatch}
+      />
     </div>
   )
 }
 
 export default Header
 
-const NavBar = ({ toggle, pathname, userAuth }) => (
-  <nav className={styles.navbar}>
-    <div className={styles.navbarContainer}>
-      <Link className={styles.navLogoLink} to="/">
-        <img src={logo} className={styles.navLogo} alt="logo" />
-      </Link>
-      <div className={styles.mobileIcon}>
-        <FaBars onClick={toggle} />
-      </div>
-      <ul className={styles.navMenu}>
-        {arrayData.map((link, key) => (
-          <li
-            className={` ${styles.navItem}  ${
-              pathname === link.path ? styles.navLinksActive : ''
-            }`}
-            key={key}
-          >
-            <Link className={styles.navLinks} to={link.path}>
-              {link.pathName}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.navBtn}>
-        {userAuth ? (
-          <Link className={styles.navBtnLink} to="/backoffice">
-            BackOffice
-          </Link>
-        ) : (
-          <Link className={styles.navBtnLink} to="/login">
-            Iniciar sesión
-          </Link>
-        )}
-      </div>
-    </div>
-  </nav>
-)
+const NavBar = ({ toggle, pathname, userAuth, dispatch }) => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <Link className={styles.navLogoLink} to="/">
+          <img src={logo} className={styles.navLogo} alt="logo" />
+        </Link>
 
-const SideBar = ({ toggle, isOpen, userAuth }) => (
+        <div className={styles.mobileIcon}>
+          <FaBars onClick={toggle} />
+        </div>
+
+        <ul className={styles.navMenu}>
+          {arrayData.map((link, key) => (
+            <li
+              className={` ${styles.navItem}  ${
+                pathname === link.path ? styles.navLinksActive : ''
+              }`}
+              key={key}
+            >
+              <Link className={styles.navLinks} to={link.path}>
+                {link.pathName}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.navBtn}>
+          {userAuth ? (
+            <>
+              <Link
+                onClick={() => dispatch(userLogout())}
+                className={styles.navBtnLink}
+                to={'/'}
+              >
+                LogOut
+              </Link>
+              <Link className={styles.navBtnLink} to={'/backoffice'}>
+                BackOffice
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to={'/login'} className={styles.navBtnLink}>
+                LogIn
+              </Link>
+              <Link to={'/register'} className={styles.navBtnLink}>
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+const SideBar = ({ toggle, isOpen, userAuth, dispatch }) => (
   <aside
     className={styles.sidebarContainer}
     style={
@@ -75,6 +103,7 @@ const SideBar = ({ toggle, isOpen, userAuth }) => (
     <div className={styles.icon}>
       <FaTimes className={styles.closeIcon} />
     </div>
+
     <div className={styles.sidebarWrapper}>
       <ul className={styles.sidebarMenu}>
         {arrayData.map((link, key) => (
@@ -85,15 +114,30 @@ const SideBar = ({ toggle, isOpen, userAuth }) => (
           </li>
         ))}
       </ul>
+
       <div className={styles.sidebarBtnWrap}>
         {userAuth ? (
-          <Link className={styles.sidebarRoute} to="/backoffice">
-            BackOffice
-          </Link>
+          <>
+            <Link
+              onClick={() => dispatch(userLogout())}
+              className={styles.sidebarRoute}
+              to={'/'}
+            >
+              LogOut
+            </Link>
+            <Link className={styles.sidebarRoute} to={'/backoffice'}>
+              BackOffice
+            </Link>
+          </>
         ) : (
-          <Link to={'/signin'} className={styles.sidebarRoute}>
-            Iniciar sesión
-          </Link>
+          <>
+            <Link to={'/login'} className={styles.sidebarRoute}>
+              LogIn
+            </Link>
+            <Link to={'/register'} className={styles.sidebarRoute}>
+              Register
+            </Link>
+          </>
         )}
       </div>
     </div>
