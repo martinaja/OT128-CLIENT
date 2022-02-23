57083ef509535660ca4ed25c2bcdc4c6fa8bd00b
Hernan <hernan.r9317@gmail.com>
2022-02-23 20:48:48 -0300

    button style

--- src/Components/Donations/Donations.js
+++ src/Components/Donations/Donations.js
@@ -1,11 +1,12 @@
+import { Container } from '@mui/material'
 import React from 'react'
 import { DonationsMp } from './DonationsMp'
 
 const Donations = () => {
   return (
-    <>
+    <Container>
       <DonationsMp />
-    </>
+    </Container>
   )
 }
 
