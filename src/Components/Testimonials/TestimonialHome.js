7c349fc3b29bdeedb078a96fb16415446203c566
Frank076-web <franco.776@hotmail.com>
2022-02-26 16:56:06 -0300

    Added image and delete button form testimonial

--- src/Components/Testimonials/TestimonialHome.js
+++ src/Components/Testimonials/TestimonialHome.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import '../CardListStyles.css'
 import { getTestimonial } from '../../features/testimonial/testimonialReducer'
 import { TestimonialGrid } from './TestimonialGrid'
+import { SkeletonArticle } from '../Skeleton/SkeletonArticle'
 
 const TestimonialHome = () => {
   const testimonialState = useSelector(
@@ -17,41 +18,41 @@ const TestimonialHome = () => {
 
   return (
     <>
+      <img src="/images/testimonios.jpeg" alt="testomonios" width="100%" />
       <Typography variant="h3" textAlign="center" mt={5}>
         Testimonios
       </Typography>
-      <Typography variant="body1" textAlign="center" p={2}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
-        blanditiis enim ipsa nemo asperiores illo, perspiciatis ratione rem non
-        corporis itaque, ea reiciendis dolorem doloribus expedita vero
-        necessitatibus quasi magni?
-      </Typography>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 550px))',
-          justifyContent: 'center',
-        }}
-      >
-        {testimonialState?.length > 0 ? (
-          testimonialState.map((testimony, index) => {
-            return (
-              <Container>
-                <TestimonialGrid
-                  id={testimony.id}
-                  link="testimonios"
-                  name={testimony.name}
-                  image={testimony.image}
-                  description={testimony.description}
-                  key={index}
-                />
-              </Container>
-            )
-          })
-        ) : (
-          <p>No se encontraron testimonios</p>
-        )}
-      </div>
+      {testimonialState?.length > 0 ? (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 550px))',
+            justifyContent: 'center',
+            marginTop: '3rem',
+          }}
+        >
+          {testimonialState?.length > 0 ? (
+            testimonialState.map((testimony, index) => {
+              return (
+                <Container>
+                  <TestimonialGrid
+                    id={testimony.id}
+                    link="testimonios"
+                    name={testimony.name}
+                    image={testimony.image}
+                    description={testimony.description}
+                    key={index}
+                  />
+                </Container>
+              )
+            })
+          ) : (
+            <p style={{ textAlign: 'center' }}>No se encontraron testimonios</p>
+          )}
+        </div>
+      ) : (
+        <SkeletonArticle />
+      )}
     </>
   )
 }
