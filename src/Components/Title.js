0bb4b78b7fc5b1462b82e4088dbba86fc75dc353
Agustín Vargas <agustinvargas93@gmail.com>
2022-02-27 16:12:19 -0300

    Remove Title default img

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import defaultImage from '../assets/defaultImage.jpg'
+// import defaultImage from '../assets/defaultImage.jpg'
 import { Box } from '@mui/material'
 
 export const Title = ({ children, image }) => {
   return (
     <Box sx={{ mt: 2 }}>
-      {/* <h1>{children}</h1> */}
-      <Box sx={{ mt: 4 }}>
-        <img width="100%" src={image || defaultImage} alt={children} />
-      </Box>
+      {image && (
+        <Box sx={{ mt: 4 }}>
+          <img width="100%" src={image} alt={children} />
+        </Box>
+      )}
       <h1
         style={{ textAlign: 'center', marginBottom: '8rem', fontSize: '3rem' }}
       >
