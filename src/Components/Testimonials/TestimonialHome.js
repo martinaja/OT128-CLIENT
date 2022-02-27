619de0262d3308d77bd3d8bbfb857451619550e6
Frank076-web <83378749+Frank076-web@users.noreply.github.com>
2022-02-27 14:50:45 -0300

    Testimony (#228)
    * updated activities and testimonials
    
    * changed cards in testimony
    
    * added link to
    
    * Added image and delete button form testimonial
    
    * added pagination and reverse method in reducer

--- src/Components/Testimonials/TestimonialHome.js
+++ src/Components/Testimonials/TestimonialHome.js
@@ -1,9 +1,10 @@
-import { Container, Typography } from '@mui/material'
-import React, { useEffect } from 'react'
+import { Container, Grid, Pagination, Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import '../CardListStyles.css'
 import { getTestimonial } from '../../features/testimonial/testimonialReducer'
 import { TestimonialGrid } from './TestimonialGrid'
+import { SkeletonArticle } from '../Skeleton/SkeletonArticle'
 
 const TestimonialHome = () => {
   const testimonialState = useSelector(
@@ -11,47 +12,70 @@ const TestimonialHome = () => {
   )
   const dispatch = useDispatch()
 
+  const [page, setPage] = useState(1)
+  const handleChange = (event, value) => {
+    setPage(value)
+  }
+  const sliceTestimonials = 6 * page
+  const startSlice = sliceTestimonials - 6
+
   useEffect(() => {
     dispatch(getTestimonial())
   }, [dispatch])
 
   return (
     <>
+      <img src="/images/testimonios.jpeg" alt="testomonios" width="100%" />
       <Typography variant="h3" textAlign="center" mt={5}>
         Testimonios
       </Typography>
-      <Typography variant="body1" textAlign="center" p={2}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
-        blanditiis enim ipsa nemo asperiores illo, perspiciatis ratione rem non
-        corporis itaque, ea reiciendis dolorem doloribus expedita vero
-        necessitatibus quasi magni?
-      </Typography>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 550px))',
-          justifyContent: 'center',
-        }}
+      {testimonialState?.length > 0 ? (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 550px))',
+            justifyContent: 'center',
+            marginTop: '3rem',
+          }}
+        >
+          {testimonialState?.length > 0 ? (
+            testimonialState
+              .slice(startSlice, sliceTestimonials)
+              .map((testimony, index) => {
+                return (
+                  <Container>
+                    <TestimonialGrid
+                      id={testimony.id}
+                      link="testimonios"
+                      name={testimony.name}
+                      image={testimony.image}
+                      description={testimony.description}
+                      key={index}
+                    />
+                  </Container>
+                )
+              })
+          ) : (
+            <p style={{ textAlign: 'center' }}>No se encontraron testimonios</p>
+          )}
+        </div>
+      ) : (
+        <SkeletonArticle />
+      )}
+      <Grid
+        Item
+        xs={12}
+        sx={{ justifyContent: 'center', display: 'flex', mb: 3 }}
       >
-        {testimonialState?.length > 0 ? (
-          testimonialState.map((testimony, index) => {
-            return (
-              <Container>
-                <TestimonialGrid
-                  id={testimony.id}
-                  link="testimonios"
-                  name={testimony.name}
-                  image={testimony.image}
-                  description={testimony.description}
-                  key={index}
-                />
-              </Container>
-            )
-          })
-        ) : (
-          <p>No se encontraron testimonios</p>
-        )}
-      </div>
+        <Stack spacing={2}>
+          <Pagination
+            color="primary"
+            count={Math.ceil(testimonialState?.length / 6)}
+            page={page}
+            onChange={handleChange}
+          />
+        </Stack>
+      </Grid>
     </>
   )
 }
