32d948b3443aa1e82bf8a45fc8cea8dd6884453f
franco-pozzi <franco663@hotmail.com>
2022-03-01 22:30:07 -0300

    Modify footer and styles

--- src/Components/PublicWeb/Footer/Footer.js
+++ src/Components/PublicWeb/Footer/Footer.js
@@ -1,5 +1,5 @@
 import { Box, Grid } from '@material-ui/core'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import style from './Footer.module.css'
 import Facebook from '@material-ui/icons/Facebook'
@@ -10,9 +10,13 @@ import LinkedIn from '@material-ui/icons/LinkedIn'
 import { useSelector, useDispatch } from 'react-redux'
 import { getOrganizationData } from '../../../features/organization/organizationReducer'
 
-import { FooterSuscribe } from '../../FooterSuscribe'
+import { FooterSuscribe } from '../../FooterSuscribe/FooterSuscribe'
 
 function Footer() {
+  const isUserSubscribe = localStorage.getItem('isUserSubscribe')
+
+  const [userSubscribe, setUserSubscribe] = useState(isUserSubscribe)
+
   const { data } = useSelector((state) => state.organization)
 
   const dispatch = useDispatch()
@@ -50,22 +54,28 @@ function Footer() {
             <Box className={style.social__tags}>
               <a className={style.social__icon} href={data.facebook_url}>
                 <Facebook size="2x" />
-                <span>Facebook</span>
+                <span>Somos_Más</span>
               </a>
               <a className={style.social__icon} href={data.twitter_url}>
                 <Twitter size="2x" />
-                <span>Twitter</span>
+                <span>Somos_Más_Arg</span>
               </a>
               <a className={style.social__icon} href={data.instagram_url}>
                 <Instagram size="2x" />
-                <span>Instagram</span>
+                <span>SomosMás</span>
               </a>
               <a className={style.social__icon} href={data.linkedin_url}>
                 <LinkedIn size="2x" />
-                <span>LinkedIn</span>
+                <span>SomosMás</span>
               </a>
             </Box>
-            <FooterSuscribe />
+            {!isUserSubscribe ? (
+              <FooterSuscribe setUserSubscribe={setUserSubscribe} />
+            ) : (
+              <h4 className={style.h4Text}>
+                ¡ Gracias ! Ya estas suscrito a las novedades.
+              </h4>
+            )}
             <div className={style.social__copyrights}>
               © 2020 Somos mas. All Rights Reserved.
             </div>
